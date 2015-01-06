f00857b52df560f19747170509e620c66c9bedeb
Artem Polyakov <artpol84@gmail.com>
2015-01-06 20:35:11 +0600

    Use autotools to build all tests

--- test/compile_cli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-PMIX_BASE=/home/artpol/WORK/Development/OpenMPI/ACTIVITY/TASKS/PMIx/libpmix/pmix
-PMIX_API=$PMIX_BASE/src/
-PMIX_INC=$PMIX_BASE/src/include
-PMIX_LIB=$PMIX_BASE/.libs/
-
-gcc -I$PMIX_BASE -I$PMIX_API -I$PMIX_INC -g -o client client.c -L$PMIX_LIB -lpmix
