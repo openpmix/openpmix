c531e1214667b3f9e3d398caeaf0178b15f79651
Artem Polyakov <artpol84@gmail.com>
2015-01-11 12:02:58 +0600

    Update copyrights.

--- test/test_common.h
+++ test/test_common.h
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2013-2015 Intel, Inc.  All rights reserved.
+ * Copyright (c) 2015      Artem Y. Polyakov <artpol84@gmail.com>.
+ *                         All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ *
+ */
+
 #ifndef TEST_COMMON_H
 #define TEST_COMMON_H
 
