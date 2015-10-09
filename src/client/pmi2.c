766c74d4e9520803bcbf3ce074f0f42c81def3d3
Artem Polyakov <artpol84@gmail.com>
2015-10-09 16:57:57 +0300

    Update copyrights according to the Git history.

--- src/client/pmi2.c
+++ src/client/pmi2.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2014-2015 Intel, Inc.  All rights reserved.
- * Copyright (c) 2014-2015 Research Organization for Information Science
+ * Copyright (c) 2015      Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
  * Copyright (c) 2015      Mellanox Technologies, Inc.
  *                         All rights reserved.
