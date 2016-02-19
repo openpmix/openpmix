a9a01d1ccac35ef26ebdd5f3ceb4dbe1ed58e99c
Igor Ivanov <Igor.Ivanov@itseez.com>
2016-02-19 13:50:18 +0200

    contrib: Change libevent download location

--- contrib/pmix_jenkins.sh
+++ contrib/pmix_jenkins.sh
@@ -191,7 +191,7 @@ if [ "$jenkins_test_build" = "yes" ]; then
     echo "Checking for build ..."
 
     cd ${WORKSPACE}/${prefix}
-    wget http://sourceforge.net/projects/levent/files/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
+    wget http://downloads.sourceforge.net/levent/libevent-2.0.22-stable.tar.gz
     tar zxf libevent-2.0.22-stable.tar.gz
     cd libevent-2.0.22-stable
     libevent_dir=$PWD/install
