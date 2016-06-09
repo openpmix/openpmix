9ae61b816643bab96acd40bcdc123b95e1ad7614
Gilles Gouaillardet <gilles@rist.or.jp>
2016-06-09 15:58:18 +0900

    server: use memcpy in parse_connect_ack() to make sparc happy with unaligned data
    (cherry picked from commit pmix/master@f91bca85af72b8f330f72a6383ebcc9bae577bf2)

--- src/server/pmix_server_listener.c
+++ src/server/pmix_server_listener.c
@@ -1,7 +1,7 @@
 /* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
  * Copyright (c) 2014-2016 Intel, Inc.  All rights reserved.
- * Copyright (c) 2014-2015 Research Organization for Information Science
+ * Copyright (c) 2014-2016 Research Organization for Information Science
  *                         and Technology (RIST). All rights reserved.
  * Copyright (c) 2014-2015 Artem Y. Polyakov <artpol84@gmail.com>.
  *                         All rights reserved.
@@ -329,7 +329,7 @@ static pmix_status_t parse_connect_ack (char *msg, int len,
 
     PMIX_STRNLEN(msglen, msg, len);
     if (msglen <= len) {
-        *rank = *(int *)msg;
+        memcpy(rank, msg, sizeof(int));
         msg += sizeof(int);
         len -= sizeof(int);
     } else {
