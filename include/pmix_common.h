176f15181d82097c6e0aa03b116f2b008d3ee4e2
Ralph Castain <rhc@open-mpi.org>
2016-08-26 14:57:39 -0700

    Add a key for #nodes in the nspace

--- include/pmix_common.h
+++ include/pmix_common.h
@@ -169,6 +169,7 @@ typedef uint32_t pmix_rank_t;
 #define PMIX_LOCAL_SIZE                     "pmix.local.size"       // (uint32_t) #procs in this job on this node
 #define PMIX_NODE_SIZE                      "pmix.node.size"        // (uint32_t) #procs across all jobs on this node
 #define PMIX_MAX_PROCS                      "pmix.max.size"         // (uint32_t) max #procs for this job
+#define PMIX_NUM_NODES                      "pmix.num.nodes"        // (uint32_t) #nodes in this nspace
 
 /* topology info */
 #define PMIX_NET_TOPO                       "pmix.ntopo"            // (char*) xml-representation of network topology
