94cdcedaf38e510537ef49c6504c2f52174334d8
David Solt <dsolt@us.ibm.com>
2016-09-01 14:47:17 -0400

    Fix an issue where event thread is killed twice resulting in segfault

--- src/tool/pmix_tool.c
+++ src/tool/pmix_tool.c
@@ -664,7 +664,15 @@ PMIX_EXPORT pmix_status_t PMIx_tool_finalize(void)
     if (0 <= pmix_client_globals.myserver.sd) {
         CLOSE_THE_SOCKET(pmix_client_globals.myserver.sd);
     }
-    event_base_free(pmix_globals.evbase);
+/*
+ * This  is already done inside of pmix_progress_thread_finalize above
+ *
+ *   if (!pmix_globals.external_evbase) {
+ *      event_base_free(pmix_globals.evbase);
+ *   }
+ */
+
+#endif
 #ifdef HAVE_LIBEVENT_GLOBAL_SHUTDOWN
     libevent_global_shutdown();
 #endif
