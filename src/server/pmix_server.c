333efe50bcf0a6433f1ec1a0efa1d6592e7042d5
Ralph Castain <rhc@open-mpi.org>
2016-11-01 14:35:09 -0700

    Backport from OMPI - have the PMIx_Client_init return the PMIx identifier when called by a PMIx server, thus allowing the server to retrieve its PMIx id.
    Silence a warning.
    
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- src/server/pmix_server.c
+++ src/server/pmix_server.c
@@ -1038,7 +1038,6 @@ PMIX_EXPORT pmix_status_t PMIx_server_setup_fork(const pmix_proc_t *proc, char *
 {
     char rankstr[128];
     pmix_listener_t *lt;
-    pmix_status_t rc;
 
     pmix_output_verbose(2, pmix_globals.debug_output,
                         "pmix:server setup_fork for nspace %s rank %d",
@@ -1114,14 +1113,14 @@ static void _dmodex_req(int sd, short args, void *cbdata)
 
     /* They are asking for job level data for this process */
     if (cd->proc.rank == PMIX_RANK_WILDCARD) {
-       
+
        data = nptr->server->job_info.base_ptr;
        sz = nptr->server->job_info.bytes_used;
 
        /* execute the callback */
        cd->cbfunc(PMIX_SUCCESS, data, sz, cd->cbdata);
        cd->active = false;
-           
+
        return;
     }
 
@@ -1667,10 +1666,10 @@ static void _spcb(int sd, short args, void *cbdata)
             }
         }
         if (NULL == nptr) {
-            /* This can happen if there are no processes from this 
-             * namespace running on this host.  In this case just 
+            /* This can happen if there are no processes from this
+             * namespace running on this host.  In this case just
              * pack the name of the namespace because we need that. */
-            msg = cd->nspace;
+            msg = (char*)cd->nspace;
             pmix_bfrop.pack(reply, &msg, 1, PMIX_STRING);
         } else {
             pmix_bfrop.copy_payload(reply, &nptr->server->job_info);
