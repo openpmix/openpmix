openpmix/openpmix

commit 333efe50bcf0a6433f1ec1a0efa1d6592e7042d5

Backport from OMPI - have the PMIx_Client_init return the PMIx identifier when called by a PMIx server, thus allowing the server to retrieve its PMIx id.
Silence a warning.

Signed-off-by: Ralph Castain <rhc@open-mpi.org>

src/server/pmix_server.c
@@ -1038,7 +1038,6 @@ PMIX_EXPORT pmix_status_t PMIx_server_setup_fork(const pmix_proc_t *proc, char *
 {
     char rankstr[128];
     pmix_listener_t *lt;
-    pmix_status_t rc;
 
     pmix_output_verbose(2, pmix_globals.debug_output,
                         "pmix:server setup_fork for nspace %s rank %d",
@@ -1114,14 +1113,14 @@ static void _dmodex_req(int sd, short args, void *cbdata)
 
     /* They are asking for job level data for this process */
     if (cd->proc.rank == PMIX_RANK_WILDCARD) {
-       
+
        data = nptr->server->job_info.base_ptr;
        sz = nptr->server->job_info.bytes_used;
 
        /* execute the callback */
        cd->cbfunc(PMIX_SUCCESS, data, sz, cd->cbdata);
        cd->active = false;
-           
+
        return;
     }
 
@@ -1667,10 +1666,10 @@ static void _spcb(int sd, short args, void *cbdata)
             }
         }
         if (NULL == nptr) {
-            /* This can happen if there are no processes from this 
-             * namespace running on this host.  In this case just 
+            /* This can happen if there are no processes from this
+             * namespace running on this host.  In this case just
              * pack the name of the namespace because we need that. */
-            msg = cd->nspace;
+            msg = (char*)cd->nspace;
             pmix_bfrop.pack(reply, &msg, 1, PMIX_STRING);
         } else {
             pmix_bfrop.copy_payload(reply, &nptr->server->job_info);

src/client/pmix_client.c
@@ -258,7 +258,7 @@ PMIX_EXPORT pmix_status_t PMIx_Init(pmix_proc_t *proc,
         return PMIX_ERR_BAD_PARAM;
     }
 
-    if (0 < pmix_globals.init_cntr) {
+    if (0 < pmix_globals.init_cntr || PMIX_PROC_SERVER == pmix_globals.proc_type) {
         /* since we have been called before, the nspace and
          * rank should be known. So return them here if
          * requested */

commit 9ff49d6ca5b5fda3733defe9866357e04f753422

dstore: fixed pmix build fails with dstore
Fixed the pmix build fail with `--dstore-enable` which introduced after the commit 333efe5.

Signed-off-by: Boris Karasev <karasev.b@gmail.com>

src/server/pmix_server.c
@@ -1038,6 +1038,9 @@ PMIX_EXPORT pmix_status_t PMIx_server_setup_fork(const pmix_proc_t *proc, char *
 {
     char rankstr[128];
     pmix_listener_t *lt;
+#if defined(PMIX_ENABLE_DSTORE) && (PMIX_ENABLE_DSTORE == 1)
+    pmix_status_t rc;
+#endif
 
     pmix_output_verbose(2, pmix_globals.debug_output,
                         "pmix:server setup_fork for nspace %s rank %d",