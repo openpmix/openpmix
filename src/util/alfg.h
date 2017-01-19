9ef627d7724a7a457a8e0a5dce1a528f6598cb5b
Ralph Castain <rhc@open-mpi.org>
2017-01-19 15:57:11 -0800

    Correct header path
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- src/util/alfg.h
+++ src/util/alfg.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2014      Mellanox Technologies, Inc.
  *                         All rights reserved.
  * Copyright (c) 2014      Cisco Systems, Inc.  All rights reserved.
- * Copyright (c) 2014-2016 Intel, Inc.  All rights reserved.
+ * Copyright (c) 2014-2017 Intel, Inc.  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -15,7 +15,7 @@
 
 #include <src/include/pmix_config.h>
 
-#include "pmix_stdint.h"
+#include "src/include/pmix_stdint.h"
 
 
 struct pmix_rng_buff_t {
