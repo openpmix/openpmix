42f54b574e021b89e036994b43646e4ddad3607b
Artem Polyakov <artpol84@gmail.com>
2017-07-10 01:57:26 +0700

    tests: Address instruction reordering issue
    In the fence test `PMIx_Get_nb` callback wasn't properly instrumented
    to ensure correct memory access.
    
    Signed-off-by: Artem Polyakov <artpol84@gmail.com>

--- test/test_common.h
+++ test/test_common.h
@@ -26,6 +26,7 @@
 #include <unistd.h>
 #include <stdint.h>
 
+#include "src/include/pmix_globals.h"
 #include "src/class/pmix_list.h"
 #include "src/util/argv.h"
 
@@ -271,6 +272,7 @@ typedef struct {
                 nanosleep(&ts,NULL);                                                                                \
                 count++;                                                                                            \
             }                                                                                                       \
+            PMIX_ACQUIRE_OBJECT(&cbdata);                                                                            \
         }                                                                                                           \
     }                                                                                                               \
     if (PMIX_SUCCESS == rc) {                                                                                       \
