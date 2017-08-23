001031f1a9726b3bf1ff760043b4531119fb385b
Ralph Castain <rhc@open-mpi.org>
2017-08-22 18:08:33 -0700

    There are times when the host RM may choose not to provide job-level info at startup - e.g., to reduce memory footprint by using some other mechanism. An example is the shared memory used by HWLOC that resides outside of PMIx. However, if such a method isn't available or doesn't succeed, the client may still need the information. So provide a mechanism by which the client can, in certain circumstances, request a piece of job-level info from the server
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- include/pmix_common.h
+++ include/pmix_common.h
@@ -234,6 +234,8 @@ typedef uint32_t pmix_rank_t;
 #define PMIX_HWLOC_SHMEM_ADDR               "pmix.hwlocaddr"        // (size_t) address of HWLOC shared memory segment
 #define PMIX_HWLOC_SHMEM_SIZE               "pmix.hwlocsize"        // (size_t) size of HWLOC shared memory segment
 #define PMIX_HWLOC_SHMEM_FILE               "pmix.hwlocfile"        // (char*) path to HWLOC shared memory file
+#define PMIX_HWLOC_XML_V1                   "pmix.hwlocxml1"        // (char*) XML representation of local topology using HWLOC v1.x format
+#define PMIX_HWLOC_XML_V2                   "pmix.hwlocxml2"        // (char*) XML representation of local topology using HWLOC v2.x format
 
 /* request-related info */
 #define PMIX_COLLECT_DATA                   "pmix.collect"          // (bool) collect data and return it at the end of the operation
