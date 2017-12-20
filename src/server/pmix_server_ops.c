c63b1528e9550c0c581f363c104db92eae2a4f94
Boris Karasev <karasev.b@gmail.com>
2017-12-20 20:15:34 +0600

    Fixed server-side get logic when there is no key in GDS
    - added the flag indicating the rank has invoked `commit`
    - the request is moved to pending till the requested rank does not invoke `commit`
    
    Signed-off-by: Boris Karasev <karasev.b@gmail.com>

--- src/server/pmix_server_ops.c
+++ src/server/pmix_server_ops.c
@@ -226,6 +226,9 @@ pmix_status_t pmix_server_commit(pmix_peer_t *peer, pmix_buffer_t *buf)
     /* mark us as having successfully received a blob from this proc */
     info->modex_recvd = true;
 
+    /* update the commit counter */
+    peer->commit_cnt++;
+
     /* see if anyone remote is waiting on this data - could be more than one */
     PMIX_LIST_FOREACH_SAFE(dcd, dcdnext, &pmix_server_globals.remote_pnd, pmix_dmdx_remote_t) {
         if (0 != strncmp(dcd->cd->proc.nspace, nptr->nspace, PMIX_MAX_NSLEN)) {
