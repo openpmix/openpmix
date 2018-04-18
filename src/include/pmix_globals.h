16705a2b1922b189c2834309ed3d1c36ade03931
Ralph Castain <rhc@open-mpi.org>
2018-04-18 09:09:49 -0700

    Transfer all cached data to client gds
    Upon first connection, the server transfers its cached nspace registration data to the gds being used by the client's nspace. Any data registered for a specific proc must go as well, even if the proc is remote. Otherwise, the client library won't find it, and won't ask the server for it because the key starts with "pmix", and must therefore have been provided at startup.
    
    Port of #707
    
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- src/include/pmix_globals.h
+++ src/include/pmix_globals.h
@@ -127,6 +127,7 @@ typedef struct pmix_personality_t {
 typedef struct {
     pmix_list_item_t super;
     char *nspace;
+    pmix_rank_t nprocs;          // num procs in this nspace
     size_t nlocalprocs;
     bool all_registered;         // all local ranks have been defined
     bool version_stored;         // the version string used by this nspace has been stored
