d2620c20061446a6f6df04e6948e6e5b5c74d733
Ralph Castain <rhc@open-mpi.org>
2019-01-03 06:41:34 -0800

    Properly cache IO for pending registrants
    Evict the oldest cached output to make room for the newest. Provide an
    MCA param for sizing the max iof cache - default it to 1M messages since
    memory is only allocated on an as-needed basis. Add some debugging
    output when pmix_server_iof_verbose is on so we can see the cache in
    operation.
    
    Signed-off-by: Ralph Castain <rhc@open-mpi.org>

--- src/server/pmix_server.c
+++ src/server/pmix_server.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
 /*
- * Copyright (c) 2014-2018 Intel, Inc.  All rights reserved.
+ * Copyright (c) 2014-2019 Intel, Inc.  All rights reserved.
  * Copyright (c) 2014-2018 Research Organization for Information Science
  *                         and Technology (RIST).  All rights reserved.
  * Copyright (c) 2014-2015 Artem Y. Polyakov <artpol84@gmail.com>.
@@ -87,18 +87,8 @@ static char *gds_mode = NULL;
 static pid_t mypid;
 
 // local functions for connection support
-static void iof_eviction_cbfunc(struct pmix_hotel_t *hotel,
-                                int room_num,
-                                void *occupant)
-{
-    pmix_setup_caddy_t *cache = (pmix_setup_caddy_t*)occupant;
-    PMIX_RELEASE(cache);
-}
-
 pmix_status_t pmix_server_initialize(void)
 {
-    pmix_status_t rc;
-
     /* setup the server-specific globals */
     PMIX_CONSTRUCT(&pmix_server_globals.clients, pmix_pointer_array_t);
     pmix_pointer_array_init(&pmix_server_globals.clients, 1, INT_MAX, 1);
@@ -109,15 +99,7 @@ pmix_status_t pmix_server_initialize(void)
     PMIX_CONSTRUCT(&pmix_server_globals.local_reqs, pmix_list_t);
     PMIX_CONSTRUCT(&pmix_server_globals.nspaces, pmix_list_t);
     PMIX_CONSTRUCT(&pmix_server_globals.groups, pmix_list_t);
-    PMIX_CONSTRUCT(&pmix_server_globals.iof, pmix_hotel_t);
-    rc = pmix_hotel_init(&pmix_server_globals.iof, PMIX_IOF_HOTEL_SIZE,
-                         pmix_globals.evbase, PMIX_IOF_MAX_STAY,
-                         iof_eviction_cbfunc);
-    if (PMIX_SUCCESS != rc) {
-       PMIX_ERROR_LOG(rc);
-       PMIX_RELEASE_THREAD(&pmix_global_lock);
-       return rc;
-    }
+    PMIX_CONSTRUCT(&pmix_server_globals.iof, pmix_list_t);
 
     pmix_output_verbose(2, pmix_server_globals.base_output,
                         "pmix:server init called");
@@ -445,7 +427,6 @@ PMIX_EXPORT pmix_status_t PMIx_server_finalize(void)
     int i;
     pmix_peer_t *peer;
     pmix_namespace_t *ns;
-    pmix_setup_caddy_t *cd;
 
     PMIX_ACQUIRE_THREAD(&pmix_global_lock);
     if (pmix_globals.init_cntr <= 0) {
@@ -473,14 +454,6 @@ PMIX_EXPORT pmix_status_t PMIx_server_finalize(void)
 
     pmix_ptl_base_stop_listening();
 
-    /* cleanout any IOF */
-    for (i=0; i < PMIX_IOF_HOTEL_SIZE; i++) {
-        pmix_hotel_checkout_and_return_occupant(&pmix_server_globals.iof, i, (void**)&cd);
-        if (NULL != cd) {
-            PMIX_RELEASE(cd);
-        }
-    }
-    PMIX_DESTRUCT(&pmix_server_globals.iof);
     for (i=0; i < pmix_server_globals.clients.size; i++) {
         if (NULL != (peer = (pmix_peer_t*)pmix_pointer_array_get_item(&pmix_server_globals.clients, i))) {
             /* ensure that we do the specified cleanup - if this is an
@@ -504,6 +477,7 @@ PMIX_EXPORT pmix_status_t PMIx_server_finalize(void)
     }
     PMIX_LIST_DESTRUCT(&pmix_server_globals.nspaces);
     PMIX_LIST_DESTRUCT(&pmix_server_globals.groups);
+    PMIX_LIST_DESTRUCT(&pmix_server_globals.iof);
 
     pmix_hwloc_cleanup();
 
@@ -1712,7 +1686,7 @@ static void _iofdeliver(int sd, short args, void *cbdata)
     pmix_buffer_t *msg;
     bool found = false;
     bool cached = false;
-    int ignore;
+    pmix_iof_cache_t *iof;
 
     pmix_output_verbose(2, pmix_server_globals.iof_output,
                         "PMIX:SERVER delivering IOF from %s on channel %0x",
@@ -1726,8 +1700,7 @@ static void _iofdeliver(int sd, short args, void *cbdata)
             continue;
         }
         /* see if the source matches the request */
-        if (0 != strncmp(cd->procs->nspace, req->pname.nspace, PMIX_MAX_NSLEN) ||
-            (PMIX_RANK_WILDCARD != req->pname.rank && cd->procs->rank != req->pname.rank)) {
+        if (!PMIX_CHECK_PROCID(cd->procs, &req->pname)) {
             continue;
         }
         /* never forward back to the source! This can happen if the source
@@ -1736,8 +1709,7 @@ static void _iofdeliver(int sd, short args, void *cbdata)
         if (NULL == req->peer->info || req->peer->finalized) {
             continue;
         }
-        if (0 == strncmp(cd->procs->nspace, req->peer->info->pname.nspace, PMIX_MAX_NSLEN) &&
-            cd->procs->rank == req->peer->info->pname.rank) {
+        if (PMIX_CHECK_PROCID(cd->procs, &req->peer->info->pname)) {
             continue;
         }
         found = true;
@@ -1778,15 +1750,21 @@ static void _iofdeliver(int sd, short args, void *cbdata)
 
     /* if nobody has registered for this yet, then cache it */
     if (!found) {
-        /* add this output to our hotel so it is cached until someone
-         * registers to receive it */
-        if (PMIX_SUCCESS != (rc = pmix_hotel_checkin(&pmix_server_globals.iof, cd, &ignore))) {
-            /* we can't cache it for some reason */
-            PMIX_ERROR_LOG(rc);
-            PMIX_RELEASE(cd);
-            return;
+        pmix_output_verbose(2, pmix_server_globals.iof_output,
+                            "PMIx:SERVER caching IOF");
+        if (pmix_server_globals.max_iof_cache == pmix_list_get_size(&pmix_server_globals.iof)) {
+            /* remove the oldest cached message */
+            iof = (pmix_iof_cache_t*)pmix_list_remove_first(&pmix_server_globals.iof);
+            PMIX_RELEASE(iof);
         }
-        cached = true;
+        /* add this output to our cache so it is cached until someone
+         * registers to receive it */
+        iof = PMIX_NEW(pmix_iof_cache_t);
+        memcpy(&iof->source, cd->procs, sizeof(pmix_proc_t));
+        iof->channel = cd->channels;
+        iof->bo = cd->bo;
+        cd->bo = NULL;  // protect the data
+        pmix_list_append(&pmix_server_globals.iof, &iof->super);
     }
 
 
