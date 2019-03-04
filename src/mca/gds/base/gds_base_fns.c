a1e466bb87c14376e195f1236c770cb74b4e3bf4
Boris Karasev <karasev.b@gmail.com>
2019-03-04 13:30:13 +0600

    modex: optimization of rank identification size reduction
    Packing the nspace string with each proc is redundant and
    not need for unpacker side. This commit avoids packing
    `nspace:rank` tuples into the modex blob, instead of that
    all ranks are renumbered throughtout procs involved in
    Modex and packed as integers.
    
    Example procs paking:
    { "nspace1", 0} : 0
    { "nspace1", 1} : 1 => procs = { {0}, {1}, {2} }
    { "nspace2", 0} : 2
    
    Signed-off-by: Boris Karasev <karasev.b@gmail.com>
    (cherry picked from commit be6ff19b1d9d3d8a536c3f4862bd3deb633291f5)
    
    Conflicts:
    	src/include/pmix_globals.h
    	src/server/pmix_server_ops.c

--- src/mca/gds/base/gds_base_fns.c
+++ src/mca/gds/base/gds_base_fns.c
@@ -24,6 +24,7 @@
 #include "src/util/error.h"
 
 #include "src/mca/gds/base/base.h"
+#include "src/server/pmix_server_ops.h"
 
 
 char* pmix_gds_base_get_available_modules(void)
@@ -104,6 +105,9 @@ pmix_status_t pmix_gds_base_store_modex(struct pmix_namespace_t *nspace,
     pmix_server_trkr_t *trk = (pmix_server_trkr_t*)cbdata;
     pmix_proc_t proc;
     pmix_buffer_t pbkt;
+    pmix_rank_t rel_rank;
+    pmix_nspace_caddy_t *nm;
+    bool found;
 
     /* Loop over the enclosed byte object envelopes and
      * store them in our GDS module */
@@ -158,8 +162,8 @@ pmix_status_t pmix_gds_base_store_modex(struct pmix_namespace_t *nspace,
             PMIX_LOAD_BUFFER(pmix_globals.mypeer, &pbkt, bo2.bytes, bo2.size);
             /* unload the proc that provided this data */
             cnt = 1;
-            PMIX_BFROPS_UNPACK(rc, pmix_globals.mypeer, &pbkt, &proc, &cnt,
-                               PMIX_PROC);
+            PMIX_BFROPS_UNPACK(rc, pmix_globals.mypeer, &pbkt, &rel_rank, &cnt,
+                               PMIX_PROC_RANK);
             if (PMIX_SUCCESS != rc) {
                 PMIX_ERROR_LOG(rc);
                 pbkt.base_ptr = NULL;
@@ -167,6 +171,29 @@ pmix_status_t pmix_gds_base_store_modex(struct pmix_namespace_t *nspace,
                 PMIX_DESTRUCT(&bkt);
                 goto error;
             }
+            found = false;
+            /* calculate proc form the relative rank */
+            if (pmix_list_get_size(&trk->nslist) == 1) {
+                found = true;
+                nm = (pmix_nspace_caddy_t*)pmix_list_get_first(&trk->nslist);
+            } else {
+                PMIX_LIST_FOREACH(nm, &trk->nslist, pmix_nspace_caddy_t) {
+                    if (rel_rank < nm->ns->nprocs) {
+                        found = true;
+                        break;
+                    }
+                    rel_rank -= nm->ns->nprocs;
+                }
+            }
+            if (false == found) {
+                rc = PMIX_ERR_NOT_FOUND;
+                PMIX_ERROR_LOG(rc);
+                pbkt.base_ptr = NULL;
+                PMIX_DESTRUCT(&pbkt);
+                PMIX_DESTRUCT(&bkt);
+                goto error;
+            }
+            PMIX_PROC_LOAD(&proc, nm->ns->nspace, rel_rank);
 
             rc = cb_fn(ctx, &proc, &pbkt);
             if (PMIX_SUCCESS != rc) {
