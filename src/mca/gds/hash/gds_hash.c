a91b8e3539279495533e8410eaaddc63fd302bd1
Boris Karasev <karasev.b@gmail.com>
2019-03-12 15:26:52 +0600

    modex: reduce modex size by replacing key names with indexes
    This commit optimizes the modex size, avoiding duplication of
    key-names for key-value pairs. It generates the key-map with
    the unique names and packs key indexes instead of key-name
    strings. The key-map packaged separately and allows the
    remote side to determine the key-name by index when unpacking.
    
    Signed-off-by: Boris Karasev <karasev.b@gmail.com>

--- src/mca/gds/hash/gds_hash.c
+++ src/mca/gds/hash/gds_hash.c
@@ -72,6 +72,7 @@ static pmix_status_t hash_store_modex(struct pmix_namespace_t *ns,
 
 static pmix_status_t _hash_store_modex(pmix_gds_base_ctx_t ctx,
                                        pmix_proc_t *proc,
+                                       char **kmap,
                                        pmix_buffer_t *pbkt);
 
 static pmix_status_t hash_fetch(const pmix_proc_t *proc,
@@ -1197,11 +1198,11 @@ static pmix_status_t hash_store_modex(struct pmix_namespace_t *nspace,
 
 static pmix_status_t _hash_store_modex(pmix_gds_base_ctx_t ctx,
                                        pmix_proc_t *proc,
+                                       char **kmap,
                                        pmix_buffer_t *pbkt)
 {
     pmix_hash_trkr_t *trk, *t;
     pmix_status_t rc = PMIX_SUCCESS;
-    int32_t cnt;
     pmix_kval_t *kv;
 
     pmix_output_verbose(2, pmix_gds_base_framework.framework_output,
@@ -1231,9 +1232,9 @@ static pmix_status_t _hash_store_modex(pmix_gds_base_ctx_t ctx,
      * contains all local participants. */
 
     /* unpack the remaining values until we hit the end of the buffer */
-    cnt = 1;
     kv = PMIX_NEW(pmix_kval_t);
-    PMIX_BFROPS_UNPACK(rc, pmix_globals.mypeer, pbkt, kv, &cnt, PMIX_KVAL);
+    rc = pmix_gds_base_modex_unpack_kval(kmap, pbkt, kv);
+
     while (PMIX_SUCCESS == rc) {
         /* store this in the hash table */
         if (PMIX_SUCCESS != (rc = pmix_hash_store(&trk->remote, proc->rank, kv))) {
@@ -1243,8 +1244,7 @@ static pmix_status_t _hash_store_modex(pmix_gds_base_ctx_t ctx,
         PMIX_RELEASE(kv);  // maintain accounting as the hash increments the ref count
         /* continue along */
         kv = PMIX_NEW(pmix_kval_t);
-        cnt = 1;
-        PMIX_BFROPS_UNPACK(rc, pmix_globals.mypeer, pbkt, kv, &cnt, PMIX_KVAL);
+        rc = pmix_gds_base_modex_unpack_kval(kmap, pbkt, kv);
     }
     PMIX_RELEASE(kv);  // maintain accounting
     if (PMIX_ERR_UNPACK_READ_PAST_END_OF_BUFFER != rc) {
