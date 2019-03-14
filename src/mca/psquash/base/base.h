349818bd99eac96036e78bac0801362ec39fadd7
Boris Karasev <karasev.b@gmail.com>
2019-03-14 12:26:41 +0600

    psquash: added flexible-size integer packing
    Signed-off-by: Boris Karasev <karasev.b@gmail.com>
    (cherry picked from commit 5c0c79d771d34434a21926f020281f56e58b39d8)

--- src/mca/psquash/base/base.h
+++ src/mca/psquash/base/base.h
@@ -1,6 +1,9 @@
 /* -*- C -*-
  *
  * Copyright (c) 2019      IBM Corporation.  All rights reserved.
+ * Copyright (c) 2019      Mellanox Technologies, Inc.
+ *                         All rights reserved.
+ *
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -25,6 +28,41 @@
 
 
 BEGIN_C_DECLS
+
+/**
+ * Sizeof by PMIx type integer values.
+ *
+ * r - return status code
+ * t - type (pmix_data_type_t) of integer value
+ * s - size of type in bytes
+ * (see a comment to `pmix_bfrops_pack_flex` for additional details)
+ */
+#define PMIX_SQUASH_TYPE_SIZEOF(r, t, s)                    \
+do {                                                        \
+    (r) = PMIX_SUCCESS;                                     \
+    switch (t) {                                            \
+        case PMIX_INT16:                                    \
+        case PMIX_UINT16:                                   \
+            (s) = SIZEOF_SHORT;                             \
+            break;                                          \
+        case PMIX_INT:                                      \
+        case PMIX_INT32:                                    \
+        case PMIX_UINT:                                     \
+        case PMIX_UINT32:                                   \
+            (s) = SIZEOF_INT;                               \
+            break;                                          \
+        case PMIX_INT64:                                    \
+        case PMIX_UINT64:                                   \
+            (s) = SIZEOF_LONG;                              \
+            break;                                          \
+        case PMIX_SIZE:                                     \
+            (s) = SIZEOF_SIZE_T;                            \
+            break;                                          \
+        default:                                            \
+            (r) = PMIX_ERR_BAD_PARAM;                       \
+    }                                                       \
+} while (0)
+
 
 PMIX_EXPORT extern pmix_mca_base_framework_t pmix_psquash_base_framework;
 
