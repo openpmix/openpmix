dc0be699b7326ec402396da89a8f975e4e08d66e
Boris Karasev <karasev.b@gmail.com>
2019-03-14 14:01:54 +0600

    psquash: rename `none` component to `native`
    Signed-off-by: Boris Karasev <karasev.b@gmail.com>
    (cherry picked from commit 37339acc36048588902618f08675c084d05f8c06)

--- src/mca/psquash/native/psquash_native.c
+++ src/mca/psquash/native/psquash_native.c
@@ -1,5 +1,7 @@
 /*
  * Copyright (c) 2019      IBM Corporation.  All rights reserved.
+ * Copyright (c) 2019      Mellanox Technologies, Inc.
+ *                         All rights reserved.
  *
  * $COPYRIGHT$
  *
@@ -24,32 +26,32 @@
 #endif
 
 #include "src/mca/psquash/psquash.h"
-#include "psquash_none.h"
+#include "psquash_native.h"
 
 #include "src/mca/bfrops/base/base.h"
 
-static pmix_status_t none_init(void);
+static pmix_status_t native_init(void);
 
-static void none_finalize(void);
+static void native_finalize(void);
 
-pmix_psquash_base_module_t pmix_none_module = {
-    .name = "none",
-    .init = none_init,
-    .finalize = none_finalize,
+pmix_psquash_base_module_t pmix_psquash_native_module = {
+    .name = "native",
+    .init = native_init,
+    .finalize = native_finalize,
     .encode_int = NULL,
     .decode_int = NULL
 };
 
 
-static pmix_status_t none_init(void)
+static pmix_status_t native_init(void)
 {
     pmix_output_verbose(2, pmix_globals.debug_output,
-                        "psquash: none init");
+                        "psquash: native init");
     return PMIX_SUCCESS;
 }
 
-static void none_finalize(void)
+static void native_finalize(void)
 {
     pmix_output_verbose(2, pmix_globals.debug_output,
-                        "psquash: none finalize");
+                        "psquash: native finalize");
 }
