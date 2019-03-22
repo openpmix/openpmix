6f08e4be9a8b46838898cec011a662b67d185156
Josh Hursey <jjhursey@open-mpi.org>
2019-03-22 12:08:21 +0700

    psquash: reword the comment
    Signed-off-by: Boris Karasev <karasev.b@gmail.com>
    (cherry picked from commit 8f6d7dc3c652dcc56f40b950c255f20e57b7ffa4)

--- src/mca/psquash/psquash.h
+++ src/mca/psquash/psquash.h
@@ -83,7 +83,7 @@ typedef pmix_status_t (*pmix_psquash_decode_int_fn_t) (pmix_data_type_t type,
  */
 typedef struct {
     const char *name;
-    /* flag of necessity to pack the type of value */
+    /* flag indicating if the type is encoded within the value, otherwise, it is necessary to further pack the type with the value. */
     bool int_type_is_encoded;
 
     /** init/finalize */
