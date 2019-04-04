2d07a6c87d7eedd1128977001ee0ffcb98f03ad6
Ralph Castain <rhc@pmix.org>
2019-04-04 09:23:46 -0700

    Implement "make check" in the tests subdir
    Add a script that runs the Jenkins tests and returns a non-zero status
    if any of them fail.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

---
+++ test/run_tests.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2019      Intel, Inc.
+#
+# $COPYRIGHT$
+#
+# Additional copyrights may follow
+
+use strict;
+
+my @tests = ("-s 2 -n 2 --job-fence",
+             "-n 4 --job-fence -c", "-n 4 --job-fence",
+             "-n 4 --ns-dist 3:1 --fence \"[db | 0:0-2;1:0]\"",
+             "-n 4 --ns-dist 3:1 --fence \"[db | 0:;1:3]\"",
+             "-n 4 --ns-dist 3:1 --fence \"[db | 0:;1:]\"",
+             "-n 4 --ns-dist 3:1 --fence \"[0:]\"",
+             "-n 4 --ns-dist 3:1 --fence \"[b | 0:]\"",
+             "-n 4 --ns-dist 3:1 --fence \"[d | 0:]\" --noise \"[0:0,1]\"",
+             "-s 2 -n 2 --job-fence -c",
+             "-n 2 --test-publish",
+             "-n 2 --test-spawn",
+             "-n 2 --test-connect",
+             "-n 5 --test-resolve-peers --ns-dist \"1:2:2\"");
+
+my $test;
+my $cmd;
+my $output;
+my $status = 0;
+
+foreach $test (@tests) {
+    $cmd = "./pmix_test " . $test . " 2>&1";
+    print $cmd . "\n";
+    $output = `$cmd`;
+    print $output . "\n";
+    print "CODE $?\n";
+    if ("0" != "$?") {
+        $status = "$?";
+    }
+}
+exit($status >> 8);
