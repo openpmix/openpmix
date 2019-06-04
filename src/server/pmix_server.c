9c4f1da7543411a455d8251291910ceae4cc5998
Ralph Castain <rhc@pmix.org>
2019-06-04 16:52:22 -0700

    Provide blocking ability to the register/deregister fns
    If the caller doesn't provide a cbfunc for PMIx_server_register_nspace
    and friends, then don't just return and let the processing of the
    request be done without pause as they could get into race conditions.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 04d83be2da72db8f98489c7ef13a2dce90c7ea40)

--- src/server/pmix_server.c
+++ src/server/pmix_server.c
@@ -495,6 +495,13 @@ PMIX_EXPORT pmix_status_t PMIx_server_finalize(void)
     return PMIX_SUCCESS;
 }
 
+static void opcbfunc(pmix_status_t status, void *cbdata)
+{
+    pmix_lock_t *lock = (pmix_lock_t*)cbdata;
+    lock->status = status;
+    PMIX_WAKEUP_THREAD(lock);
+}
+
 static void _register_nspace(int sd, short args, void *cbdata)
 {
     pmix_setup_caddy_t *cd = (pmix_setup_caddy_t*)cbdata;
@@ -554,9 +561,7 @@ static void _register_nspace(int sd, short args, void *cbdata)
                             cd->info, cd->ninfo);
 
   release:
-    if (NULL != cd->opcbfunc) {
-        cd->opcbfunc(rc, cd->cbdata);
-    }
+    cd->opcbfunc(rc, cd->cbdata);
     PMIX_RELEASE(cd);
 }
 
@@ -566,6 +571,8 @@ PMIX_EXPORT pmix_status_t PMIx_server_register_nspace(const pmix_nspace_t nspace
                                                       pmix_op_cbfunc_t cbfunc, void *cbdata)
 {
     pmix_setup_caddy_t *cd;
+    pmix_status_t rc;
+    pmix_lock_t mylock;
 
     PMIX_ACQUIRE_THREAD(&pmix_global_lock);
     if (pmix_globals.init_cntr <= 0) {
@@ -583,6 +590,22 @@ PMIX_EXPORT pmix_status_t PMIx_server_register_nspace(const pmix_nspace_t nspace
     if (0 < ninfo) {
         cd->ninfo = ninfo;
         cd->info = info;
+    }
+
+    /* if the provided callback is NULL, then substitute
+     * our own internal cbfunc and block here */
+    if (NULL == cbfunc) {
+        PMIX_CONSTRUCT_LOCK(&mylock);
+        cd->opcbfunc = opcbfunc;
+        cd->cbdata = &mylock;
+        PMIX_THREADSHIFT(cd, _register_nspace);
+        PMIX_WAIT_THREAD(&mylock);
+        rc = mylock.status;
+        PMIX_DESTRUCT_LOCK(&mylock);
+        if (PMIX_SUCCESS == rc) {
+            rc = PMIX_OPERATION_SUCCEEDED;
+        }
+        return rc;
     }
 
     /* we have to push this into our event library to avoid
@@ -708,9 +731,7 @@ static void _deregister_nspace(int sd, short args, void *cbdata)
     }
 
     /* release the caller */
-    if (NULL != cd->opcbfunc) {
-        cd->opcbfunc(rc, cd->cbdata);
-    }
+    cd->opcbfunc(rc, cd->cbdata);
     PMIX_RELEASE(cd);
 }
 
@@ -719,6 +740,7 @@ PMIX_EXPORT void PMIx_server_deregister_nspace(const pmix_nspace_t nspace,
                                                void *cbdata)
 {
     pmix_setup_caddy_t *cd;
+    pmix_lock_t mylock;
 
     pmix_output_verbose(2, pmix_server_globals.base_output,
                         "pmix:server deregister nspace %s",
@@ -738,6 +760,18 @@ PMIX_EXPORT void PMIx_server_deregister_nspace(const pmix_nspace_t nspace,
     PMIX_LOAD_PROCID(&cd->proc, nspace, PMIX_RANK_WILDCARD);
     cd->opcbfunc = cbfunc;
     cd->cbdata = cbdata;
+
+    /* if the provided callback is NULL, then substitute
+     * our own internal cbfunc and block here */
+    if (NULL == cbfunc) {
+        PMIX_CONSTRUCT_LOCK(&mylock);
+        cd->opcbfunc = opcbfunc;
+        cd->cbdata = &mylock;
+        PMIX_THREADSHIFT(cd, _deregister_nspace);
+        PMIX_WAIT_THREAD(&mylock);
+        PMIX_DESTRUCT_LOCK(&mylock);
+        return;
+    }
 
     /* we have to push this into our event library to avoid
      * potential threading issues */
@@ -1015,9 +1049,7 @@ static void _register_client(int sd, short args, void *cbdata)
 
   cleanup:
     /* let the caller know we are done */
-    if (NULL != cd->opcbfunc) {
-        cd->opcbfunc(rc, cd->cbdata);
-    }
+    cd->opcbfunc(rc, cd->cbdata);
     PMIX_RELEASE(cd);
 }
 
@@ -1026,6 +1058,8 @@ PMIX_EXPORT pmix_status_t PMIx_server_register_client(const pmix_proc_t *proc,
                                                       pmix_op_cbfunc_t cbfunc, void *cbdata)
 {
     pmix_setup_caddy_t *cd;
+    pmix_status_t rc;
+    pmix_lock_t mylock;
 
     PMIX_ACQUIRE_THREAD(&pmix_global_lock);
     if (pmix_globals.init_cntr <= 0) {
@@ -1049,6 +1083,22 @@ PMIX_EXPORT pmix_status_t PMIx_server_register_client(const pmix_proc_t *proc,
     cd->server_object = server_object;
     cd->opcbfunc = cbfunc;
     cd->cbdata = cbdata;
+
+    /* if the provided callback is NULL, then substitute
+     * our own internal cbfunc and block here */
+    if (NULL == cbfunc) {
+        PMIX_CONSTRUCT_LOCK(&mylock);
+        cd->opcbfunc = opcbfunc;
+        cd->cbdata = &mylock;
+        PMIX_THREADSHIFT(cd, _register_client);
+        PMIX_WAIT_THREAD(&mylock);
+        rc = mylock.status;
+        PMIX_DESTRUCT_LOCK(&mylock);
+        if (PMIX_SUCCESS == rc) {
+            rc = PMIX_OPERATION_SUCCEEDED;
+        }
+        return rc;
+    }
 
     /* we have to push this into our event library to avoid
      * potential threading issues */
@@ -1130,9 +1180,7 @@ static void _deregister_client(int sd, short args, void *cbdata)
     }
 
   cleanup:
-    if (NULL != cd->opcbfunc) {
-        cd->opcbfunc(PMIX_SUCCESS, cd->cbdata);
-    }
+    cd->opcbfunc(PMIX_SUCCESS, cd->cbdata);
     PMIX_RELEASE(cd);
 }
 
@@ -1140,6 +1188,7 @@ PMIX_EXPORT void PMIx_server_deregister_client(const pmix_proc_t *proc,
                                                pmix_op_cbfunc_t cbfunc, void *cbdata)
 {
     pmix_setup_caddy_t *cd;
+    pmix_lock_t mylock;
 
     PMIX_ACQUIRE_THREAD(&pmix_global_lock);
     if (pmix_globals.init_cntr <= 0) {
@@ -1166,6 +1215,18 @@ PMIX_EXPORT void PMIx_server_deregister_client(const pmix_proc_t *proc,
     cd->proc.rank = proc->rank;
     cd->opcbfunc = cbfunc;
     cd->cbdata = cbdata;
+
+    /* if the provided callback is NULL, then substitute
+     * our own internal cbfunc and block here */
+    if (NULL == cbfunc) {
+        PMIX_CONSTRUCT_LOCK(&mylock);
+        cd->opcbfunc = opcbfunc;
+        cd->cbdata = &mylock;
+        PMIX_THREADSHIFT(cd, _deregister_client);
+        PMIX_WAIT_THREAD(&mylock);
+        PMIX_DESTRUCT_LOCK(&mylock);
+        return;
+    }
 
     /* we have to push this into our event library to avoid
      * potential threading issues */
