0447cab60ddd6b7b2d6fce16cf956738cc6fa875
Gilles Gouaillardet <gilles@rist.or.jp>
2020-01-06 13:37:59 +0900

    psensor/base: make it re-initializable
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

--- src/mca/psensor/base/psensor_base_select.c
+++ src/mca/psensor/base/psensor_base_select.c
@@ -10,6 +10,8 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2016-2017 Intel, Inc. All rights reserved.
+ * Copyright (c) 2020      Research Organization for Information Science
+ *                         and Technology (RIST).  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -27,8 +29,6 @@
 
 #include "src/mca/psensor/base/base.h"
 
-static bool selected = false;
-
 /* Function for selecting a prioritized list of components
  * from all those that are available. */
 int pmix_psensor_base_select(void)
@@ -40,11 +40,11 @@ int pmix_psensor_base_select(void)
     int pri;
     bool inserted;
 
-    if (selected) {
+    if (pmix_psensor_base.selected) {
         /* ensure we don't do this twice */
         return PMIX_SUCCESS;
     }
-    selected = true;
+    pmix_psensor_base.selected = true;
 
     /* Query all available components and ask if they have a module */
     PMIX_LIST_FOREACH(cli, &pmix_psensor_base_framework.framework_components, pmix_mca_base_component_list_item_t) {
