1349045309be55977149f1d5e7d0cbb7cac584d4
Gilles Gouaillardet <gilles@rist.or.jp>
2020-01-06 13:16:31 +0900

    psec/base: make it re-initializable
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

--- src/mca/psec/base/psec_base_frame.c
+++ src/mca/psec/base/psec_base_frame.c
@@ -12,8 +12,8 @@
  *                         All rights reserved.
  * Copyright (c) 2012-2013 Los Alamos National Security, Inc.  All rights reserved.
  * Copyright (c) 2014-2017 Intel, Inc. All rights reserved.
- * Copyright (c) 2015-2016 Research Organization for Information Science
- *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2015-2020 Research Organization for Information Science
+ *                         and Technology (RIST).  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -54,6 +54,7 @@ static pmix_status_t pmix_psec_close(void)
         return PMIX_SUCCESS;
     }
     pmix_psec_globals.initialized = false;
+    pmix_psec_globals.selected = false;
 
     PMIX_LIST_FOREACH_SAFE(active, prev, &pmix_psec_globals.actives, pmix_psec_base_active_module_t) {
       pmix_list_remove_item(&pmix_psec_globals.actives, &active->super);
--- src/mca/psec/base/psec_base_select.c
+++ src/mca/psec/base/psec_base_select.c
@@ -10,6 +10,8 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2016-2017 Intel, Inc. All rights reserved.
+ * Copyright (c) 2020      Research Organization for Information Science
+ *                         and Technology (RIST).  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -29,8 +31,6 @@
 
 #include "src/mca/psec/base/base.h"
 
-static bool selected = false;
-
 /* Function for selecting a prioritized list of components
  * from all those that are available. */
 int pmix_psec_base_select(void)
@@ -43,11 +43,11 @@ int pmix_psec_base_select(void)
     int rc, priority;
     bool inserted;
 
-    if (selected) {
+    if (pmix_psec_globals.selected) {
         /* ensure we don't do this twice */
         return PMIX_SUCCESS;
     }
-    selected = true;
+    pmix_psec_globals.selected = true;
 
     /* Query all available components and ask if they have a module */
     PMIX_LIST_FOREACH(cli, &pmix_psec_base_framework.framework_components, pmix_mca_base_component_list_item_t) {
