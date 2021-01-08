d5ac0681699979d41ea564f24314ac6b32888541
Joshua Hursey <jhursey@us.ibm.com>
2021-01-08 12:35:15 -0500

    Fix Python binding build for VPATH
     * We need to include both the autogenerated include directory
       (in the VPATH) and the static include directory (in src/include)
       when building the python bindings.
    
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>
    (cherry picked from commit 522ba04ff8da0cc9663371221d68ef4c52eb5516)
    (cherry picked from commit 25cd2eec2b513c5f55b9780b2c85ed05876c9e99)

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -2,14 +2,36 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 from sys import platform, maxsize, version_info
-import os
-from Cython.Compiler.Main import default_options
+import os, sys
+from Cython.Compiler.Main import default_options, CompilationOptions
 default_options['emit_linenums'] = True
 from subprocess import check_output, CalledProcessError
 
+def get_include():
+    dirs = []
+    try:
+        dirs.append(os.environ['PMIX_TOP_BUILDDIR'] + "/include")
+        dirs.append(os.environ['PMIX_TOP_SRCDIR'] + "/include")
+    except:
+        return dirs
+    return dirs
+    
 def getVersion():
     dir = os.path.dirname(__file__)
+
     vers_path = os.path.join(dir, '../../include', 'pmix_version.h')
+    if not os.path.exists(vers_path):
+        include_dirs = get_include()
+        vers_path = None
+        for dir in include_dirs:
+            tmp_path = os.path.join(dir, 'pmix_version.h')
+            if os.path.exists(tmp_path):
+                vers_path = tmp_path
+                break
+        if vers_path is None:
+            print("Error: pmix_version.h does not exist at path: ",vers_path)
+            sys.exit(1)
+
     with open(vers_path) as verFile:
         lines = verFile.readlines()
         for l in lines:
@@ -44,6 +66,9 @@ setup(
             'Programming Language :: Python :: 3.6'],
     keywords = 'PMI PMIx HPC MPI SHMEM',
     platforms = 'any',
-    ext_modules = cythonize([Extension("pmix", ["pmix.pyx"], libraries=["pmix"])],
-                            compiler_directives={'language_level': 3})
+    ext_modules = cythonize([Extension("pmix",
+                                       [os.environ['PMIX_TOP_SRCDIR']+"/bindings/python/pmix.pyx"],
+                                       libraries=["pmix"]) ],
+                            compiler_directives={'language_level': 3}),
+    include_dirs = get_include()
 )
