3608dae97f22f8a0398ae9a958246ea10708362c
David Wootton <dwootton@us.ibm.com>
2021-03-03 15:21:49 -0500

    Initialize myproc structure before calling PMIx_tool_set_server
    Signed-off-by: David Wootton <dwootton@us.ibm.com>
    (cherry picked from commit 24150703ef7e9b78261d70c16dd685d70f4aef52)
    (cherry picked from commit 0ce76d5d5cadecf1baf0ee7337ee3c48b96a1baa)

--- src/server/pmix_server.c
+++ src/server/pmix_server.c
@@ -349,7 +349,6 @@ PMIX_EXPORT pmix_status_t PMIx_server_init(pmix_server_module_t *module,
     pmix_cmd_t cmd;
     pmix_cb_t cb;
     pmix_value_t value;
-    pmix_proc_t myproc;
     pmix_lock_t reglock, releaselock;
     pmix_status_t code;
 
@@ -682,7 +681,7 @@ PMIX_EXPORT pmix_status_t PMIx_server_init(pmix_server_module_t *module,
             return rc;
         }
         /* restore our original primary server */
-        rc = PMIx_tool_set_server(&myproc, NULL, 0);
+        rc = PMIx_tool_set_server(&pmix_globals.myid, NULL, 0);
         if (PMIX_SUCCESS != rc) {
             return rc;
         }
