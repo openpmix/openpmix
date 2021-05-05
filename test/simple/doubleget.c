06a191034045c32615c54512a0471de53dc54fc9
Ralph Castain <rhc@pmix.org>
2021-05-04 20:14:10 -0700

    Minor touchups
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- test/simple/doubleget.c
+++ test/simple/doubleget.c
@@ -1,22 +1,13 @@
-#include <stdio.h>
+#define _GNU_SOURCE
 #include "include/pmix.h"
 #include <sched.h>
+#include <stdio.h>
 
 static pmix_proc_t allproc = {0};
 static pmix_proc_t myproc = {0};
 static bool mywait = false;
 static bool refresh = false;
 static bool timeout = false;
-
-#define ERR(msg, ...)							\
-    do {								\
-	time_t tm = time(NULL);						\
-	char *stm = ctime(&tm);						\
-	stm[strlen(stm)-1] = 0;						\
-	fprintf(stderr, "%s ERROR: %s:%d  " msg "\n", stm, __FILE__, __LINE__, ## __VA_ARGS__); \
-	exit(1);							\
-    } while(0);
-
 
 static int pmi_set_string(const char *key, void *data, size_t size)
 {
@@ -26,26 +17,28 @@ static int pmi_set_string(const char *key, void *data, size_t size)
     PMIX_VALUE_CONSTRUCT(&value);
     value.type = PMIX_BYTE_OBJECT;
     value.data.bo.bytes = data;
-    value.data.bo.size  = size;
+    value.data.bo.size = size;
     if (PMIX_SUCCESS != (rc = PMIx_Put(PMIX_GLOBAL, key, &value))) {
-        ERR("Client ns %s rank %d: PMIx_Put failed: %s\n", myproc.nspace, myproc.rank, PMIx_Error_string(rc));
+        fprintf(stderr, "Client ns %s rank %d: PMIx_Put failed: %s\n", myproc.nspace, myproc.rank,
+                PMIx_Error_string(rc));
     }
 
     if (PMIX_SUCCESS != (rc = PMIx_Commit())) {
-        ERR("Client ns %s rank %d: PMIx_Commit failed: %s\n", myproc.nspace, myproc.rank, PMIx_Error_string(rc));
+        fprintf(stderr, "Client ns %s rank %d: PMIx_Commit failed: %s\n", myproc.nspace,
+                myproc.rank, PMIx_Error_string(rc));
     }
 
     /* protect the data */
     value.data.bo.bytes = NULL;
-    value.data.bo.size  = 0;
+    value.data.bo.size = 0;
     PMIX_VALUE_DESTRUCT(&value);
     printf("%s:%d PMIx_Put on %s\n", myproc.nspace, myproc.rank, key);
-
 
     return 0;
 }
 
-static int pmi_get_string(uint32_t peer_rank, const char *key, void **data_out, size_t *data_size_out)
+static int pmi_get_string(uint32_t peer_rank, const char *key, void **data_out,
+                          size_t *data_size_out)
 {
     int rc;
     pmix_proc_t proc;
@@ -66,10 +59,12 @@ static int pmi_get_string(uint32_t peer_rank, const char *key, void **data_out, 
         rc = PMIx_Get(&proc, key, NULL, 0, &pvalue);
     }
     if (PMIX_SUCCESS != rc) {
-        ERR("Client ns %s rank %d: PMIx_Get on rank %u %s: %s\n", myproc.nspace, myproc.rank, peer_rank, key, PMIx_Error_string(rc));
+        fprintf(stderr, "Client ns %s rank %d: PMIx_Get on rank %u %s: %s\n", myproc.nspace,
+                myproc.rank, peer_rank, key, PMIx_Error_string(rc));
     }
-    if(pvalue->type != PMIX_BYTE_OBJECT){
-        ERR("Client ns %s rank %d: PMIx_Get %s: got wrong data type\n", myproc.nspace, myproc.rank, key);
+    if (pvalue->type != PMIX_BYTE_OBJECT) {
+        fprintf(stderr, "Client ns %s rank %d: PMIx_Get %s: got wrong data type\n", myproc.nspace,
+                myproc.rank, key);
     }
     *data_out = pvalue->data.bo.bytes;
     *data_size_out = pvalue->data.bo.size;
@@ -80,7 +75,8 @@ static int pmi_get_string(uint32_t peer_rank, const char *key, void **data_out, 
     PMIX_VALUE_RELEASE(pvalue);
     PMIX_PROC_DESTRUCT(&proc);
 
-    printf("%s:%d PMIx_get %s returned %zi bytes\n", myproc.nspace, myproc.rank, key, data_size_out[0]);
+    printf("%s:%d PMIx_get %s returned %zi bytes\n", myproc.nspace, myproc.rank, key,
+           data_size_out[0]);
 
     return 0;
 }
@@ -94,12 +90,12 @@ static int pmix_exchange(bool flag)
     PMIX_INFO_CONSTRUCT(&info);
     PMIX_INFO_LOAD(&info, PMIX_COLLECT_DATA, &flag, PMIX_BOOL);
     rc = PMIx_Fence(&allproc, 1, &info, 1);
-    if (PMIX_SUCCESS != rc){
-        fprintf(stderr, "Client ns %s rank %d: PMIx_Fence_nb failed: %d\n", myproc.nspace, myproc.rank, rc);
+    if (PMIX_SUCCESS != rc) {
+        fprintf(stderr, "Client ns %s rank %d: PMIx_Fence_nb failed: %d\n", myproc.nspace,
+                myproc.rank, rc);
         exit(1);
     }
     PMIX_INFO_DESTRUCT(&info);
-
 
     return 0;
 }
@@ -138,7 +134,7 @@ int main(int argc, char *argv[])
     }
 
     if (PMIX_SUCCESS != (rc = PMIx_Init(&myproc, NULL, 0))) {
-	   ERR("PMIx_Init failed");
+        fprintf(stderr, "PMIx_Init failed");
         exit(1);
     }
     if (myproc.rank == 0) {
@@ -152,7 +148,8 @@ int main(int argc, char *argv[])
 
     /* get the number of procs in our job */
     if (PMIX_SUCCESS != (rc = PMIx_Get(&allproc, PMIX_JOB_SIZE, NULL, 0, &pvalue))) {
-        fprintf(stderr, "Client ns %s rank %d: PMIx_Get job size failed: %d\n", myproc.nspace, myproc.rank, rc);
+        fprintf(stderr, "Client ns %s rank %d: PMIx_Get job size failed: %d\n", myproc.nspace,
+                myproc.rank, rc);
         exit(1);
     }
     PMIX_VALUE_RELEASE(pvalue);
@@ -178,16 +175,18 @@ int main(int argc, char *argv[])
     }
 
     if (0 == myproc.rank) {
-        pmi_get_string(1, "test-key-3", (void**)&data_out, &size_out);
+        pmi_get_string(1, "test-key-3", (void **) &data_out, &size_out);
     } else {
-        pmi_get_string(0, "test-key-2", (void**)&data_out, &size_out);
+        pmi_get_string(0, "test-key-2", (void **) &data_out, &size_out);
     }
     printf("%d: obtained data \"%s\"\n", myproc.rank, data_out);
 
     if (PMIX_SUCCESS != (rc = PMIx_Finalize(NULL, 0))) {
-        ERR("Client ns %s rank %d:PMIx_Finalize failed: %d\n", myproc.nspace, myproc.rank, rc);
+        fprintf(stderr, "Client ns %s rank %d:PMIx_Finalize failed: %d\n", myproc.nspace,
+                myproc.rank, rc);
     }
-    if(myproc.rank == 0) printf("PMIx finalized\n");
+    if (myproc.rank == 0)
+        printf("PMIx finalized\n");
 
     exit(0);
 }
--- test/simple/hybrid.c
+++ test/simple/hybrid.c
@@ -3,9 +3,8 @@
 #include <pmix.h>
 
 //<EG BEGIN ID="declare_model_cb">
-static void model_declared_cb(size_t evhdlr_registration_id,
-                              pmix_status_t status, const pmix_proc_t *source,
-                              pmix_info_t info[], size_t ninfo,
+static void model_declared_cb(size_t evhdlr_registration_id, pmix_status_t status,
+                              const pmix_proc_t *source, pmix_info_t info[], size_t ninfo,
                               pmix_info_t results[], size_t nresults,
                               pmix_event_notification_cbfunc_fn_t cbfunc, void *cbdata)
 {
@@ -25,19 +24,12 @@ static void model_declared_cb(size_t evhdlr_registration_id,
         /* tell the event handler that we are only a partial step */
         cbfunc(PMIX_EVENT_PARTIAL_ACTION_TAKEN, NULL, 0, NULL, NULL, cbdata);
     }
-  }
-  if (NULL != cbfunc) {
-    /* tell the event handler that we are only a partial step */
-    cbfunc(PMIX_EVENT_PARTIAL_ACTION_TAKEN, NULL, 0, NULL, NULL, cbdata);
-  }
 }
 //<EG END ID="declare_model_cb">
 
 //<EG BEGIN ID="omp_thread">
-static void parallel_region_OMP_cb(size_t evhdlr_registration_id,
-                                   pmix_status_t status,
-                                   const pmix_proc_t *source,
-                                   pmix_info_t info[], size_t ninfo,
+static void parallel_region_OMP_cb(size_t evhdlr_registration_id, pmix_status_t status,
+                                   const pmix_proc_t *source, pmix_info_t info[], size_t ninfo,
                                    pmix_info_t results[], size_t nresults,
                                    pmix_event_notification_cbfunc_fn_t cbfunc, void *cbdata)
 {
@@ -51,20 +43,19 @@ static void parallel_region_OMP_cb(size_t evhdlr_registration_id,
 //<EG END ID="omp_thread">
 
 //<EG BEGIN ID="mpi_thread">
-static void parallel_region_MPI_cb(size_t evhdlr_registration_id,
-                                   pmix_status_t status,
-                                   const pmix_proc_t *source,
-                                   pmix_info_t info[], size_t ninfo,
+static void parallel_region_MPI_cb(size_t evhdlr_registration_id, pmix_status_t status,
+                                   const pmix_proc_t *source, pmix_info_t info[], size_t ninfo,
                                    pmix_info_t results[], size_t nresults,
                                    pmix_event_notification_cbfunc_fn_t cbfunc, void *cbdata)
 {
     printf("Entered %s\n", __func__);
     /* do what we need MPI to do on entering a parallel region */
-    cbfunc(PMIX_EVENT_ACTION_COMPLETE, NULL, 0, NULL, NULL, cbdata);
-  }
+    if (NULL != cbfunc) {
+        /* do what we need MPI to do on entering a parallel region */
+        cbfunc(PMIX_EVENT_ACTION_COMPLETE, NULL, 0, NULL, NULL, cbdata);
+    }
 }
 //<EG END ID="mpi_thread">
-
 
 static int openmp_handler(void)
 {
@@ -115,7 +106,7 @@ static int mpi_handler(void)
 
 static void notify_complete(pmix_status_t status, void *cbdata)
 {
-    volatile bool *flag = (volatile bool*)cbdata;
+    volatile bool *flag = (volatile bool *) cbdata;
     *flag = true;
 }
 
@@ -126,35 +117,36 @@ int main(int argc, char **argv)
     pmix_info_t *info;
     volatile bool wearedone = false;
 
-  PMIX_INFO_CREATE(info, 4);
-  PMIX_INFO_LOAD(&info[0], PMIX_PROGRAMMING_MODEL, "MPI", PMIX_STRING);
-  PMIX_INFO_LOAD(&info[1], PMIX_MODEL_LIBRARY_NAME, "FooMPI", PMIX_STRING);
-  PMIX_INFO_LOAD(&info[2], PMIX_MODEL_LIBRARY_VERSION, "1.0.0", PMIX_STRING);
-  PMIX_INFO_LOAD(&info[3], PMIX_THREADING_MODEL, "pthread", PMIX_STRING);
-  pmix_status_t rc = PMIx_Init(&myproc, info, 4);
-  PMIX_INFO_FREE(info, 4);
-  //<EG END ID="declare_model">
-  printf("Registered MPI programming model\n");
+    PMIX_INFO_CREATE(info, 4);
+    PMIX_INFO_LOAD(&info[0], PMIX_PROGRAMMING_MODEL, "MPI", PMIX_STRING);
+    PMIX_INFO_LOAD(&info[1], PMIX_MODEL_LIBRARY_NAME, "FooMPI", PMIX_STRING);
+    PMIX_INFO_LOAD(&info[2], PMIX_MODEL_LIBRARY_VERSION, "1.0.0", PMIX_STRING);
+    PMIX_INFO_LOAD(&info[3], PMIX_THREADING_MODEL, "pthread", PMIX_STRING);
+    pmix_status_t rc = PMIx_Init(&myproc, info, 4);
+    PMIX_INFO_FREE(info, 4);
+    //<EG END ID="declare_model">
+    printf("Registered MPI programming model\n");
 
-  printf("Registering event handler for model declaration\n");
-  //<EG BEGIN ID="declare_model_cb">
-  pmix_status_t code = PMIX_MODEL_DECLARED;
-  rc = PMIx_Register_event_handler(&code, 1, NULL, 0, model_declared_cb, NULL, NULL);
-  //<EG END ID="declare_model_cb">
+    printf("Registering event handler for model declaration\n");
+    //<EG BEGIN ID="declare_model_cb">
+    pmix_status_t code = PMIX_MODEL_DECLARED;
+    rc = PMIx_Register_event_handler(&code, 1, NULL, 0, model_declared_cb, NULL, NULL);
+    //<EG END ID="declare_model_cb">
     if (rc < 0) {
         fprintf(stderr, "Failed to register event handler for model declaration\n");
         goto fin;
     }
-  printf("Registered event handler for model declaration\n");
+    printf("Registered event handler for model declaration\n");
 
-  openmp_handler();
-  mpi_handler();
+    openmp_handler();
+    mpi_handler();
 
-  printf("Notifying OpenMP parallel region about to be entered\n");
-  //<EG BEGIN ID="notify_event">
+    printf("Notifying OpenMP parallel region about to be entered\n");
+    //<EG BEGIN ID="notify_event">
     PMIX_INFO_CREATE(info, 1);
     PMIX_INFO_LOAD(&info[0], PMIX_EVENT_NON_DEFAULT, NULL, PMIX_BOOL);
-  rc = PMIx_Notify_event(PMIX_OPENMP_PARALLEL_ENTERED, &myproc, PMIX_RANGE_PROC_LOCAL, info, 1, notify_complete, (void*)&wearedone);
+    rc = PMIx_Notify_event(PMIX_OPENMP_PARALLEL_ENTERED, &myproc, PMIX_RANGE_PROC_LOCAL, info, 1,
+                           notify_complete, (void *) &wearedone);
     if (rc < 0) {
         fprintf(stderr, "Failed to notify OpenMP region entered\n");
         goto fin;
@@ -170,5 +162,5 @@ int main(int argc, char **argv)
 
 fin:
     PMIx_Finalize(NULL, 0);
-  //<EG END ID="notify_event">
+    //<EG END ID="notify_event">
 }
