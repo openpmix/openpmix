29294282c4367d84426ffa797930e795f89b1272
Brian Barrett <bbarrett@amazon.com>
2022-02-04 12:44:10 -0800

    Be defensive against string literals in env
    Add two new functions to the pmix_environ code: pmix_getenv() and
    pmix_environ_merge_inplace().  The first is an implementation of
    getenv() with a second parameter to be explicit about the environment
    to search.  The second adds the contents of an environment-style
    key=value argv array into another environment, not overwriting
    the values of existing keys.  The search does not modify
    the added environment keys, protecting PMIx from string literals
    or other read-only data in environ when building application
    environments.
    
    Update the pfexec code to use pmix_environ_merge_inplace().
    
    Add a test to make sure the new code works (and test some of the
    existing code, although there is still room for improvement).
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>
    (cherry picked from commit ec0daf39030f0b3e66e1cddd43f5fa92fc1fb4f0)

---
+++ test/pmix_environ.c
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2022      Amazon.com, Inc. or its affiliates.
+ *                         All Rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ *
+ */
+
+#include "src/include/pmix_config.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "src/util/pmix_environ.h"
+#include "src/util/argv.h"
+
+int main(int argc, char *argv[])
+{
+    char **test_array;
+    char **new_array;
+    pmix_status_t ret;
+    int ret_int;
+
+    /*
+     * mess with environ
+     */
+
+    /* first test: does overwrite work? */
+    ret = pmix_setenv("pmix_environ_test_string_3", "three", true, &environ);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_setenv returned %d\n", ret);
+        return 1;
+    }
+    ret = pmix_setenv("pmix_environ_test_string_3", "four", false, &environ);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_setenv returned %d\n", ret);
+        return 1;
+    }
+    if (strcmp(getenv("pmix_environ_test_string_3"), "three") != 0) {
+        printf("pmix_setenv overwrote.  Found %s expected %s\n",
+               getenv("pmix_environ_test_string_3"), "three");
+        return 1;
+    }
+    ret = pmix_setenv("pmix_environ_test_string_3", "four", true, &environ);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_setenv returned %d\n", ret);
+        return 1;
+    }
+    if (strcmp(getenv("pmix_environ_test_string_3"), "four") != 0) {
+        printf("pmix_setenv overwrote.  Found %s expected %s\n",
+               getenv("pmix_environ_test_string_3"), "four");
+        return 1;
+    }
+
+    /* second test: do we handle environ properly in unsetenv */
+    ret_int = putenv("pmix_environ_test_string_1=one");
+    if (ret_int != 0) {
+        printf("putenv() returned %d\n", ret_int);
+        return 1;
+    }
+    ret_int = setenv("pmix_environ_test_string_2", "two", true);
+    if (ret_int != 0) {
+        printf("setenv() returned %d\n", ret_int);
+        return 1;
+    }
+    ret = pmix_setenv("pmix_environ_test_string_3", "three", true, &environ);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_setenv returned %d\n", ret);
+        return 1;
+    }
+    ret = pmix_unsetenv("pmix_environ_test_string_1", &environ);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_unsetenv returned %d\n", ret);
+        return 1;
+    }
+    ret = pmix_unsetenv("pmix_environ_test_string_2", &environ);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_unsetenv returned %d\n", ret);
+        return 1;
+    }
+    ret = pmix_unsetenv("pmix_environ_test_string_3", &environ);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_unsetenv returned %d\n", ret);
+        return 1;
+    }
+
+    /* third test: can we merge environ into a new env.  Note that the
+     * new env is empty, so we can compare environ and newenv later
+     * and they should match.  We'll test the actual merging later
+     * without environ as the additions, so we have more control over
+     * the situation */
+    ret_int = putenv("pmix_environ_test_string_1=one");
+    if (ret_int != 0) {
+        printf("putenv() returned %d\n", ret_int);
+        return 1;
+    }
+    ret_int = setenv("pmix_environ_test_string_2", "two", true);
+    if (ret_int != 0) {
+        printf("setenv() returned %d\n", ret_int);
+        return 1;
+    }
+    ret = pmix_setenv("pmix_environ_test_string_3", "three", true, &environ);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_setenv returned %d\n", ret);
+        return 1;
+    }
+    test_array = NULL;
+    ret = pmix_environ_merge_inplace(&test_array, environ);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_environ_merge returned %d\n", ret);
+        return 1;
+    }
+    pmix_argv_free(test_array);
+
+    /* fourth test: make sure environ_merge_inplace does. */
+    test_array = NULL;
+    pmix_argv_append_nosize(&test_array, "pmix_environ_test_string_1=old_one");
+    pmix_argv_append_nosize(&test_array, "pmix_environ_test_string_2=old_two");
+    pmix_argv_append_nosize(&test_array, "pmix_environ_test_string_3=old_three");
+    pmix_argv_append_nosize(&test_array, "pmix_environ_test_string_4=old_four");
+
+    new_array = NULL;
+    pmix_argv_append_nosize(&new_array, "pmix_environ_test_string_1=new_one");
+    pmix_argv_append_nosize(&new_array, "pmix_environ_test_string_5=new_five");
+
+    ret = pmix_environ_merge_inplace(&test_array, new_array);
+    if (ret != PMIX_SUCCESS) {
+        printf("pmix_environ_merge returned %d\n", ret);
+        return 1;
+    }
+
+    if (NULL == pmix_getenv("pmix_environ_test_string_1", test_array)) {
+        printf("new env missing string 1\n");
+        return 1;
+    }
+    if (NULL == pmix_getenv("pmix_environ_test_string_2", test_array)) {
+        printf("new env missing string 2\n");
+        return 1;
+    }
+    if (NULL == pmix_getenv("pmix_environ_test_string_3", test_array)) {
+        printf("new env missing string 3\n");
+        return 1;
+    }
+    if (NULL == pmix_getenv("pmix_environ_test_string_4", test_array)) {
+        printf("new env missing string 4\n");
+        return 1;
+    }
+    if (NULL == pmix_getenv("pmix_environ_test_string_5", test_array)) {
+        printf("new env missing string 5\n");
+        return 1;
+    }
+
+    if (strcmp("old_one", pmix_getenv("pmix_environ_test_string_1", test_array)) != 0) {
+        printf("wrong value.  expected %s, found %s\n",
+               "old_one", pmix_getenv("pmix_environ_test_string_1", test_array));
+        return 1;
+    }
+    if (strcmp("old_two", pmix_getenv("pmix_environ_test_string_2", test_array)) != 0) {
+        printf("wrong value.  expected %s, found %s\n",
+               "old_two", pmix_getenv("pmix_environ_test_string_2", test_array));
+        return 1;
+    }
+    if (strcmp("old_three", pmix_getenv("pmix_environ_test_string_3", test_array)) != 0) {
+        printf("wrong value.  expected %s, found %s\n",
+               "old_three", pmix_getenv("pmix_environ_test_string_3", test_array));
+        return 1;
+    }
+    if (strcmp("old_four", pmix_getenv("pmix_environ_test_string_4", test_array)) != 0) {
+        printf("wrong value.  expected %s, found %s\n",
+               "old_four", pmix_getenv("pmix_environ_test_string_4", test_array));
+        return 1;
+    }
+    if (strcmp("new_five", pmix_getenv("pmix_environ_test_string_5", test_array)) != 0) {
+        printf("wrong value.  expected %s, found %s\n",
+               "new_five", pmix_getenv("pmix_environ_test_string_5", test_array));
+        return 1;
+    }
+
+    if (5 != pmix_argv_count(test_array)) {
+        printf("test array has wrong length.  found %d, expected %d\n",
+               pmix_argv_count(test_array), 5);
+        return 1;
+    }
+
+    pmix_argv_free(test_array);
+
+    return 0;
+}
