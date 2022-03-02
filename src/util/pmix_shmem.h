eabe5f546530eef3891d40d51b45adcdc7f96d37
Samuel K. Gutierrez <samuel@lanl.gov>
2022-03-02 15:28:07 -0700

    Add shared-memory segment utility functions.
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

---
+++ src/util/pmix_shmem.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021-2022 Triad National Security, LLC. All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#ifndef PMIX_UTIL_SHMEM_H
+#define PMIX_UTIL_SHMEM_H
+
+#include "src/include/pmix_config.h"
+#include "include/pmix_common.h"
+
+typedef struct pmix_shmem_t {
+    /* Size of shared-memory segment. */
+    size_t size;
+    /* Base address of shared memory segment. */
+    uintptr_t base_address;
+    /* Buffer holding path to backing store. */
+    char backing_path[PMIX_PATH_MAX];
+} pmix_shmem_t;
+
+PMIX_EXPORT pmix_status_t
+pmix_shmem_segment_create(
+    pmix_shmem_t *shmem,
+    size_t size,
+    const char *backing_path
+);
+
+PMIX_EXPORT void *
+pmix_shmem_segment_attach(
+    pmix_shmem_t *shmem,
+    void *base_address
+);
+
+PMIX_EXPORT pmix_status_t
+pmix_shmem_segment_detach(
+    pmix_shmem_t *shmem
+);
+
+PMIX_EXPORT pmix_status_t
+pmix_shmem_segment_unlink(
+    pmix_shmem_t *shmem
+);
+
+#endif
