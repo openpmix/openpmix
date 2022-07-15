0daf8e2537928c06479dbf73c420425262b4ca5b
Ralph Castain <rhc@pmix.org>
2022-07-15 06:24:16 -0700

    Fix show_help output to include tools
    Ensure that show_help is distributed to attached tools.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/mca/plog/default/plog_default.c
+++ src/mca/plog/default/plog_default.c
@@ -43,16 +43,18 @@ static pmix_status_t mylog(const pmix_proc_t *source, const pmix_info_t data[], 
                            void *cbdata);
 
 /* Module def */
-pmix_plog_module_t pmix_plog_default_module = {.name = "default",
-                                               .channels = NULL,
-                                               .init = init,
-                                               .finalize = NULL,
-                                               .log = mylog};
+pmix_plog_module_t pmix_plog_default_module = {
+    .name = "default",
+    .channels = NULL,
+    .init = init,
+    .finalize = NULL,
+    .log = mylog
+};
 
 /* local object */
 typedef struct {
     pmix_object_t super;
-    pmix_info_t *data;
+    const pmix_info_t *data;
     size_t ndata;
     pmix_op_cbfunc_t cbfunc;
     void *cbdata;
@@ -62,13 +64,8 @@ static void lcon(local_caddy_t *p)
     p->data = NULL;
     p->ndata = 0;
 }
-static void ldes(local_caddy_t *p)
-{
-    if (NULL != p->data) {
-        PMIX_INFO_FREE(p->data, p->ndata);
-    }
-}
-static PMIX_CLASS_INSTANCE(local_caddy_t, pmix_object_t, lcon, ldes);
+static PMIX_CLASS_INSTANCE(local_caddy_t, pmix_object_t,
+                           lcon, NULL);
 
 static int init(void)
 {
@@ -94,19 +91,6 @@ static pmix_status_t mylog(const pmix_proc_t *source, const pmix_info_t data[], 
                            void *cbdata)
 {
     local_caddy_t *cd;
-    size_t ntodo, n;
-
-    /* if none of the prior modules performed a requested logging
-     * operation, then we will try here */
-    ntodo = 0;
-    for (n = 0; n < ndata; n++) {
-        if (!PMIX_INFO_OP_IS_COMPLETE(&data[n])) {
-            ++ntodo;
-        }
-    }
-    if (0 == ntodo) {
-        return PMIX_ERR_TAKE_NEXT_OPTION;
-    }
 
     /* send it upwards for potential handling. This might seem
      * odd in the case where we are a gateway, but we must allow
@@ -116,23 +100,10 @@ static pmix_status_t mylog(const pmix_proc_t *source, const pmix_info_t data[], 
     if (NULL == cd) {
         return PMIX_ERR_NOMEM;
     }
+    cd->data = data;
+    cd->ndata = ndata;
     cd->cbfunc = cbfunc;
     cd->cbdata = cbdata;
-
-    /* separate out the ones that weren't completed */
-    PMIX_INFO_CREATE(cd->data, ntodo);
-    if (NULL == cd->data) {
-        PMIX_RELEASE(cd);
-        return PMIX_ERR_NOMEM;
-    }
-    cd->ndata = ntodo;
-    ntodo = 0;
-    for (n = 0; n < ndata; n++) {
-        if (!PMIX_INFO_OP_IS_COMPLETE(&data[n])) {
-            PMIX_INFO_XFER(&cd->data[ntodo], (pmix_info_t *) &data[n]);
-            ++ntodo;
-        }
-    }
 
     /* ask the host to log the remainder */
     pmix_host_server.log(source, cd->data, cd->ndata, directives, ndirs, localcbfn, (void *) cd);
