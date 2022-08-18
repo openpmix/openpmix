584090ef71e7229803b5b4ee73953c4409749c1a
Ralph Castain <rhc@pmix.org>
2022-08-18 15:19:22 -0700

    Silence the gcc12 complaints
    Just need to encapsulate nspace names in pmix_nspace_t. We
    already were consistent in use of char* for keys.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- test/simple/simpfabric.c
+++ test/simple/simpfabric.c
@@ -108,8 +108,11 @@ int main(int argc, char **argv)
     pmix_cpuset_t mycpuset;
     pmix_device_distance_t *distances;
     size_t ndist;
-    pmix_device_type_t type = PMIX_DEVTYPE_OPENFABRICS | PMIX_DEVTYPE_NETWORK | PMIX_DEVTYPE_COPROC
-                              | PMIX_DEVTYPE_GPU;
+    pmix_device_type_t type = PMIX_DEVTYPE_OPENFABRICS |
+                              PMIX_DEVTYPE_NETWORK |
+                              PMIX_DEVTYPE_COPROC |
+                              PMIX_DEVTYPE_GPU;
+    pmix_nspace_t ncache;
     PMIX_HIDE_UNUSED_PARAMS(argc, argv);
 
     /* smoke test */
@@ -200,10 +203,10 @@ int main(int argc, char **argv)
     PMIX_INFO_LOAD(&info[1], PMIX_ALLOC_NETWORK_SEC_KEY, NULL, PMIX_BOOL);
 
     PMIX_INFO_LOAD(&iptr[3], PMIX_SETUP_APP_ENVARS, NULL, PMIX_BOOL);
-
+    PMIX_LOAD_NSPACE(ncache, "SIMPSCHED");
     DEBUG_CONSTRUCT_LOCK(&cd.lock);
-    if (PMIX_SUCCESS
-        != (rc = PMIx_server_setup_application("SIMPSCHED", iptr, 4, setup_cbfunc, &cd))) {
+    rc = PMIx_server_setup_application(ncache, iptr, 4, setup_cbfunc, &cd);
+    if (PMIX_SUCCESS != rc) {
         pmix_output(0, "[%s:%d] PMIx_server_setup_application failed: %s", __FILE__, __LINE__,
                     PMIx_Error_string(rc));
         DEBUG_DESTRUCT_LOCK(&cd.lock);
@@ -214,9 +217,8 @@ int main(int argc, char **argv)
 
     /* setup the local subsystem */
     DEBUG_CONSTRUCT_LOCK(&lock);
-    if (PMIX_SUCCESS
-        != (rc = PMIx_server_setup_local_support("SIMPSCHED", cd.info, cd.ninfo, local_cbfunc,
-                                                 &lock))) {
+    rc = PMIx_server_setup_local_support(ncache, cd.info, cd.ninfo, local_cbfunc, &lock);
+    if (PMIX_SUCCESS != rc) {
         pmix_output(0, "[%s:%d] PMIx_server_setup_local_support failed: %s", __FILE__, __LINE__,
                     PMIx_Error_string(rc));
         DEBUG_DESTRUCT_LOCK(&lock);
