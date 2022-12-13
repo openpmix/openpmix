7806440d584610c7e455c3b42203eaf07b1cc623
Ralph Castain <rhc@pmix.org>
2022-12-12 16:49:59 -0800

    Complete the macro-to-function conversion
    Convert the remaining data type support macros.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 3efea8768dc3aebb55c1de6063adcf24059f6f5f)

--- src/mca/bfrops/base/bfrop_base_macro_backers.c
+++ src/mca/bfrops/base/bfrop_base_macro_backers.c
@@ -606,12 +606,6 @@ pmix_value_cmp_t PMIx_Value_compare(pmix_value_t *v1,
 }
 
 
-void PMIx_Data_array_destruct(pmix_data_array_t *d)
-{
-    pmix_bfrops_base_darray_destruct(d);
-}
-
-
 PMIX_EXPORT void PMIx_Info_construct(pmix_info_t *p)
 {
     PMIx_Load_key(p->key, NULL);
@@ -620,7 +614,9 @@ PMIX_EXPORT void PMIx_Info_construct(pmix_info_t *p)
 
 PMIX_EXPORT void PMIx_Info_destruct(pmix_info_t *p)
 {
-    PMIx_Value_destruct(&p->value);
+    if (!PMIx_Info_is_persistent(p)) {
+        PMIx_Value_destruct(&p->value);
+    }
 }
 
 PMIX_EXPORT pmix_info_t* PMIx_Info_create(size_t n)
@@ -1518,3 +1514,441 @@ void PMIx_Node_stats_free(pmix_node_stats_t *p, size_t n)
         }
     }
 }
+
+void PMIx_Pdata_construct(pmix_pdata_t *p)
+{
+    memset(p, 0, sizeof(pmix_pdata_t));
+    p->value.type = PMIX_UNDEF;
+}
+
+void PMIx_Pdata_destruct(pmix_pdata_t *p)
+{
+    PMIx_Value_destruct(&p->value);
+}
+
+pmix_pdata_t* PMIx_Pdata_create(size_t n)
+{
+    pmix_pdata_t *p;
+    size_t m;
+
+    if (0 == n) {
+        return NULL;
+    }
+    p = (pmix_pdata_t*)pmix_malloc(n * sizeof(pmix_pdata_t));
+    if (NULL != p) {
+        for (m=0; m < n; m++) {
+            PMIx_Pdata_construct(&p[m]);
+        }
+    }
+    return p;
+}
+
+void PMIx_Pdata_free(pmix_pdata_t *p, size_t n)
+{
+    size_t m;
+
+    if (NULL != p) {
+        for (m=0; m < n; m++) {
+            PMIx_Pdata_destruct(&p[m]);
+        }
+    }
+}
+
+void PMIx_App_construct(pmix_app_t *p)
+{
+    memset(p, 0, sizeof(pmix_app_t));
+}
+
+void PMIx_App_destruct(pmix_app_t *p)
+{
+    if (NULL != p->cmd) {
+        pmix_free(p->cmd);
+        p->cmd = NULL;
+    }
+    if (NULL != p->argv) {
+        PMIx_Argv_free(p->argv);
+        p->argv = NULL;
+    }
+    if (NULL != p->env) {
+        PMIx_Argv_free(p->env);
+        p->env = NULL;
+    }
+    if (NULL != p->cwd) {
+        pmix_free(p->cwd);
+        p->cwd = NULL;
+    }
+    if (NULL != p->info) {
+        PMIX_INFO_FREE(p->info, p->ninfo);
+        p->info = NULL;
+        p->ninfo = 0;
+    }
+}
+
+pmix_app_t* PMIx_App_create(size_t n)
+{
+    pmix_app_t *p;
+    size_t m;
+
+    if (0 == n) {
+        return NULL;
+    }
+    p = (pmix_app_t*)pmix_malloc(n * sizeof(pmix_app_t));
+    if (NULL != p) {
+        for (m=0; m < n; m++) {
+            PMIx_App_construct(&p[m]);
+        }
+    }
+    return p;
+}
+
+void PMIx_App_info_create(pmix_app_t *p, size_t n)
+{
+    p->ninfo = n;
+    p->info = PMIx_Info_create(n);
+}
+
+void PMIx_App_free(pmix_app_t *p, size_t n)
+{
+    size_t m;
+
+    if (NULL != p) {
+        for (m=0; m < n; m++) {
+            PMIx_App_destruct(&p[m]);
+        }
+    }
+}
+
+void PMIx_App_release(pmix_app_t *p)
+{
+    PMIx_App_free(p, 1);
+}
+
+
+void PMIx_Query_construct(pmix_query_t *p)
+{
+    memset(p, 0, sizeof(pmix_query_t));
+}
+
+void PMIx_Query_destruct(pmix_query_t *p)
+{
+    if (NULL != p->keys) {
+        PMIx_Argv_free(p->keys);
+    }
+    if (NULL != p->qualifiers) {
+        PMIx_Info_free(p->qualifiers, p->nqual);
+        pmix_free(p->qualifiers);
+        p->qualifiers = NULL;
+        p->nqual = 0;
+    }
+}
+
+pmix_query_t* PMIx_Query_create(size_t n)
+{
+    pmix_query_t *p;
+    size_t m;
+
+    if (0 == n) {
+        return NULL;
+    }
+    p = (pmix_query_t*)pmix_malloc(n * sizeof(pmix_query_t));
+    if (NULL != p) {
+        for (m=0; m < n; m++) {
+            PMIx_Query_construct(&p[m]);
+        }
+    }
+    return p;
+}
+void PMIx_Query_qualifiers_create(pmix_query_t *p, size_t n)
+{
+    p->nqual = n;
+    p->qualifiers = PMIx_Info_create(n);
+}
+
+void PMIx_Query_free(pmix_query_t *p, size_t n)
+{
+    size_t m;
+
+    if (NULL != p) {
+        for (m=0; m < n; m++) {
+            PMIx_Query_destruct(&p[m]);
+        }
+    }
+}
+
+void PMIx_Query_release(pmix_query_t *p)
+{
+    PMIx_Query_free(p, 1);
+}
+
+
+void PMIx_Regattr_construct(pmix_regattr_t *p)
+{
+    p->name = NULL;
+    PMIx_Load_key(p->string, NULL);
+    p->type = PMIX_UNDEF;
+    p->description = NULL;
+}
+
+void PMIx_Regattr_destruct(pmix_regattr_t *p)
+{
+    if (NULL != p) {
+        if (NULL != p->name) {
+            pmix_free(p->name);
+        }
+        if (NULL != p->description) {
+            PMIx_Argv_free(p->description);
+        }
+    }
+}
+
+pmix_regattr_t* PMIx_Regattr_create(size_t n)
+{
+    pmix_regattr_t *p;
+    size_t m;
+
+    if (0 == n) {
+        return NULL;
+    }
+    p = (pmix_regattr_t*)pmix_malloc(n * sizeof(pmix_regattr_t));
+    if (NULL != p) {
+        for (m=0; m < n; m++) {
+            PMIx_Regattr_construct(&p[m]);
+        }
+    }
+    return p;
+}
+
+void PMIx_Regattr_free(pmix_regattr_t *p, size_t n)
+{
+    size_t m;
+
+    if (NULL != p) {
+        for (m=0; m < n; m++) {
+            PMIx_Regattr_destruct(&p[m]);
+        }
+    }
+}
+
+void PMIx_Regattr_load(pmix_regattr_t *p,
+                       const char *name,
+                       const char *key,
+                       pmix_data_type_t type,
+                       const char *description)
+{
+    if (NULL != name) {
+        p->name = strdup(name);
+    }
+    if (NULL != key) {
+        PMIx_Load_key(p->string, key);
+    }
+    p->type = type;
+    if (NULL != description) {
+        PMIx_Argv_append_nosize(&p->description, description);
+    }
+}
+
+void PMIx_Regattr_xfer(pmix_regattr_t *dest,
+                       const pmix_regattr_t *src)
+{
+    PMIx_Regattr_construct(dest);
+    if (NULL != (src->name)) {
+        dest->name = strdup(src->name);
+    }
+    PMIx_Load_key(dest->string, src->string);
+    dest->type = src->type;
+    if (NULL != src->description) {
+        dest->description = PMIx_Argv_copy(src->description);
+    }
+}
+
+
+void PMIx_Data_array_init(pmix_data_array_t *p,
+                          pmix_data_type_t type)
+{
+    p->array = NULL;
+    p->type = type;
+    p->size = 0;
+}
+
+void PMIx_Data_array_construct(pmix_data_array_t *p,
+                               size_t num, pmix_data_type_t type)
+{
+    p->type = type;
+    p->size = num;
+    if (0 < num) {
+        if (PMIX_INFO == type) {
+            p->array = PMIx_Info_create(num);
+
+        } else if (PMIX_PROC == type) {
+            p->array = PMIx_Proc_create(num);
+
+        } else if (PMIX_PROC_INFO == type) {
+            p->array = PMIx_Proc_info_create(num);
+
+        } else if (PMIX_ENVAR == type) {
+            p->array = PMIx_Envar_create(num);
+
+        } else if (PMIX_VALUE == type) {
+            p->array = PMIx_Value_create(num);
+
+        } else if (PMIX_PDATA == type) {
+            p->array = PMIx_Pdata_create(num);
+
+        } else if (PMIX_QUERY == type) {
+            p->array = PMIx_Query_create(num);
+
+        } else if (PMIX_APP == type) {
+            p->array = PMIx_App_create(num);
+
+        } else if (PMIX_BYTE_OBJECT == type ||
+                   PMIX_COMPRESSED_STRING == type) {
+            p->array = PMIx_Byte_object_create(num);
+
+        } else if (PMIX_ALLOC_DIRECTIVE == type ||
+                   PMIX_PROC_STATE == type ||
+                   PMIX_PERSIST == type ||
+                   PMIX_SCOPE == type ||
+                   PMIX_DATA_RANGE == type ||
+                   PMIX_BYTE == type ||
+                   PMIX_INT8 == type ||
+                   PMIX_UINT8 == type ||
+                   PMIX_POINTER == type) {
+            p->array = pmix_malloc(num * sizeof(int8_t));
+            memset(p->array, 0, num * sizeof(int8_t));
+
+        } else if (PMIX_STRING == type) {
+            p->array = pmix_malloc(num * sizeof(char*));
+            memset(p->array, 0, num * sizeof(char*));
+
+        } else if (PMIX_SIZE == type) {
+            p->array = pmix_malloc(num * sizeof(size_t));
+            memset(p->array, 0, num * sizeof(size_t));
+
+        } else if (PMIX_PID == type) {
+            p->array = pmix_malloc(num * sizeof(pid_t));
+            memset(p->array, 0, num * sizeof(pid_t));
+
+        } else if (PMIX_INT == type ||
+                   PMIX_UINT == type ||
+                   PMIX_STATUS == type) {
+            p->array = pmix_malloc(num * sizeof(int));
+            memset(p->array, 0, num * sizeof(int));
+
+        } else if (PMIX_IOF_CHANNEL == type ||
+                   PMIX_DATA_TYPE == type ||
+                   PMIX_INT16 == type ||
+                   PMIX_UINT16 == type) {
+            p->array = pmix_malloc(num * sizeof(int16_t));
+            memset(p->array, 0, num * sizeof(int16_t));
+
+        } else if (PMIX_PROC_RANK == type ||
+                   PMIX_INFO_DIRECTIVES == type ||
+                   PMIX_INT32 == type ||
+                   PMIX_UINT32 == type) {
+            p->array = pmix_malloc(num * sizeof(int32_t));
+            memset(p->array, 0, num * sizeof(int32_t));
+
+        } else if (PMIX_INT64 == type ||
+                   PMIX_UINT64 == type) {
+            p->array = pmix_malloc(num * sizeof(int64_t));
+            memset(p->array, 0, num * sizeof(int64_t));
+
+        } else if (PMIX_FLOAT == type) {
+            p->array = pmix_malloc(num * sizeof(float));
+            memset(p->array, 0, num * sizeof(float));
+
+        } else if (PMIX_DOUBLE == type) {
+            p->array = pmix_malloc(num * sizeof(double));
+            memset(p->array, 0, num * sizeof(double));
+
+        } else if (PMIX_TIMEVAL == type) {
+            p->array = pmix_malloc(num * sizeof(struct timeval));
+            memset(p->array, 0, num * sizeof(struct timeval));
+
+        } else if (PMIX_TIME == type) {
+            p->array = pmix_malloc(num * sizeof(time_t));
+            memset(p->array, 0, num * sizeof(time_t));
+
+        } else if (PMIX_REGATTR == type) {
+            p->array = PMIx_Regattr_create(num);
+
+        } else if (PMIX_BOOL == type) {
+            p->array = pmix_malloc(num * sizeof(bool));
+            memset(p->array, 0, num * sizeof(bool));
+
+        } else if (PMIX_COORD == type) {
+            /* cannot use PMIx_Coord_create as we do not
+             * know the number of dimensions */
+            p->array = pmix_malloc(num * sizeof(pmix_coord_t));
+            memset(p->array, 0, num * sizeof(pmix_coord_t));
+
+        } else if (PMIX_LINK_STATE == type) {
+            p->array = pmix_malloc(num * sizeof(pmix_link_state_t));
+            memset(p->array, 0, num * sizeof(pmix_link_state_t));
+
+        } else if (PMIX_ENDPOINT == type) {
+            p->array = PMIx_Endpoint_create(num);
+
+        } else if (PMIX_PROC_NSPACE == type) {
+            p->array = pmix_malloc(num * sizeof(pmix_nspace_t));
+            memset(p->array, 0, num * sizeof(pmix_nspace_t));
+
+        } else if (PMIX_PROC_STATS == type) {
+            p->array = PMIx_Proc_stats_create(num);
+
+        } else if (PMIX_DISK_STATS == type) {
+            p->array = PMIx_Disk_stats_create(num);
+
+        } else if (PMIX_NET_STATS == type) {
+            p->array = PMIx_Net_stats_create(num);
+
+        } else if (PMIX_NODE_STATS == type) {
+            p->array = PMIx_Node_stats_create(num);
+
+        } else if (PMIX_DEVICE_DIST == type) {
+            p->array = PMIx_Device_distance_create(num);
+
+        } else if (PMIX_GEOMETRY == type) {
+            p->array = PMIx_Geometry_create(num);
+
+        } else if (PMIX_REGATTR == type) {
+            p->array = PMIx_Regattr_create(num);
+
+        } else if (PMIX_PROC_CPUSET == type) {
+            p->array = PMIx_Cpuset_create(num);
+
+        } else {
+            p->array = NULL;
+            p->size = 0;
+        }
+    } else {
+        p->array = NULL;
+    }
+}
+
+void PMIx_Data_array_destruct(pmix_data_array_t *d)
+{
+    pmix_bfrops_base_darray_destruct(d);
+}
+
+PMIX_EXPORT pmix_data_array_t* PMIx_Data_array_create(size_t n, pmix_data_type_t type)
+{
+    pmix_data_array_t *p;
+
+    if (0 == n) {
+        return NULL;
+    }
+    p = (pmix_data_array_t*)pmix_malloc(sizeof(pmix_data_array_t));
+    if (NULL != p) {
+        PMIx_Data_array_construct(p, n, type);
+    }
+    return p;
+}
+
+PMIX_EXPORT void PMIx_Data_array_free(pmix_data_array_t *p)
+{
+    if (NULL != p) {
+        PMIx_Data_array_destruct(p);
+    }
+}
+
