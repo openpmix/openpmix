openpmix/openpmix

commit 9cd0aa4f7c1bedee50c6fcaa482b0f1eaca6ea0d

Silence a warning during Python binding install
Use pip to do the install, modifying the setup.py
to support that model.

Thanks to Sam Gutierrez for showing how to do it.

Signed-off-by: Ralph Castain <rhc@pmix.org>

bindings/python/Makefile.am
@@ -37,8 +37,14 @@ install-exec-local: $(helpers)
 	$(PYTHON) $(top_srcdir)/bindings/python/construct.py --src="$(top_builddir)/include" --include-dir="$(top_srcdir)/include"
 	PMIX_BINDINGS_TOP_SRCDIR=$(PMIX_TOP_SRCDIR) PMIX_BINDINGS_TOP_BUILDDIR=$(PMIX_TOP_BUILDDIR) \
 		$(PYTHON) $(top_srcdir)/bindings/python/setup.py build_ext --library-dirs="$(DESTDIR)$(libdir)" --user
+		if [ $(PMIX_TOP_SRCDIR) != $(PMIX_TOP_BUILDDIR) ]; then \
+			pushd $(PMIX_TOP_BUILDDIR)/bindings/python; \
+			rm -f setup.py; \
+			cp $(PMIX_TOP_SRCDIR)/bindings/python/setup.py .; \
+			popd; \
+		fi
 	PMIX_BINDINGS_TOP_SRCDIR=$(PMIX_TOP_SRCDIR) PMIX_BINDINGS_TOP_BUILDDIR=$(PMIX_TOP_BUILDDIR) \
-		$(PYTHON) $(top_srcdir)/bindings/python/setup.py install --prefix="$(DESTDIR)$(prefix)"
+		$(PYTHON) -m pip install --prefix="$(DESTDIR)$(prefix)" --editable $(top_builddir)/bindings/python
 
 uninstall-hook:
 	rm -f $(pythondir)/pmix*.so

bindings/python/setup.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2022      Nanook Consulting. All rights reserved
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
@@ -48,33 +52,52 @@ def getVersion():
         version = ".".join(vers)
         return version
 
-setup(
-    name = 'pypmix',
-    version = getVersion(),
-    url = 'https://pmix.org',
-    license = '3-clause BSD',
-    author = 'Ralph H. Castain',
-    author_email = 'rhc@pmix.org',
-    description = 'Python bindings for PMIx',
-    classifiers = [
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Developers',
-            'Topic :: HPC :: Parallel Programming :: System Management',
-            'License :: OSI Approved :: BSD License',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: 3.12'],
-    keywords = 'PMI PMIx HPC MPI SHMEM',
-    platforms = 'any',
-    ext_modules = cythonize([Extension("pmix",
-                                       [os.environ['PMIX_BINDINGS_TOP_SRCDIR']+"/bindings/python/pmix.pyx"],
-                                       libraries=["pmix"]) ],
-                            compiler_directives={'language_level': 3}),
-    include_dirs = get_include()
-)
+def package_setup(package_name, package_vers):
+    '''
+    Package setup routine.
+    '''
+    setup(
+        name = 'pypmix',
+        version = getVersion(),
+        url = 'https://pmix.org',
+        license = '3-clause BSD',
+        author = 'Ralph H. Castain',
+        author_email = 'rhc@pmix.org',
+        description = 'Python bindings for PMIx',
+        classifiers = [
+                'Development Status :: 5 - Production/Stable',
+                'Intended Audience :: Developers',
+                'Topic :: HPC :: Parallel Programming :: System Management',
+                'License :: OSI Approved :: BSD License',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python :: 3.7',
+                'Programming Language :: Python :: 3.8',
+                'Programming Language :: Python :: 3.9',
+                'Programming Language :: Python :: 3.10',
+                'Programming Language :: Python :: 3.11',
+                'Programming Language :: Python :: 3.12'],
+        keywords = 'PMI PMIx HPC MPI SHMEM',
+        platforms = 'any',
+        ext_modules = cythonize([Extension("pmix",
+                                           [os.environ['PMIX_BINDINGS_TOP_SRCDIR']+"/bindings/python/pmix.pyx"],
+                                           libraries=["pmix"]) ],
+                                compiler_directives={'language_level': 3}),
+        include_dirs = get_include()
+    )
+
+def main():
+    '''
+    The main entry point for this program.
+    '''
+    package_name = 'pypmix'
+    package_vers = getVersion()
+
+    package_setup(package_name, package_vers)
+
+    return os.EX_OK
+
+
+if __name__ == '__main__':
+    sys.exit(main())

bindings/python/construct.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+#
+# Copyright (c) 2022      Nanook Consulting. All rights reserved
 
 import os, os.path, sys, shutil, signal
 from optparse import OptionParser, OptionGroup

bindings/python/pmix.pxi
@@ -1,3 +1,8 @@
+#file: pmix.pxi
+#
+# Copyright (c) 2022      Nanook Consulting. All rights reserved
+#
+
 from libc.string cimport memset, strncpy, strcpy, strlen, strdup
 from libc.stdlib cimport malloc, realloc, free
 from libc.string cimport memcpy

bindings/python/pmix.pyx
@@ -1,4 +1,6 @@
 #file: pmix.pyx
+#
+# Copyright (c) 2022      Nanook Consulting. All rights reserved
 
 from libc.string cimport memset, strncpy, strcpy, strlen, strdup
 

commit 81c0e9db75bb4247cf1ecd0fe0ae60333014fbc8

python: updates for Python bindings
- Separate build vs. install: the Python bindings will now be built
  during "make all", and installed during "make install" (vs. being
  built and installed during "make install").
  - Fix regression introduced in 9cd0aa4f where the Python bindings
    were not actually installed in $prefix.
- Update from Python distutils to setuptools.  We still use setup.py
  instead of the newer setup.cfg or pyproject.toml, but that can be
  upgraded later.
  - Suppress "setup.py" deprecated warnings during the installation.
- Fix keywords listing in the Python package
- Setup dependencies properly so that the Python bindings will
  properly re-build if any of its dependent header files change.
- Use a workaround to make Cython work properly in VPATH builds.
- Update "make uninstall" for the Python bindings to be a bit more
  robust.
- Make "make clean" remove the generated constants files.
- Removed some stale kruft from setup.py; updated to use setuptools
  instead of disttools.
- Add bindings/python/requirements.txt

Signed-off-by: Jeff Squyres <jeff@squyres.com>

bindings/python/Makefile.am
@@ -23,36 +23,123 @@
 # $HEADER$
 #
 
-# we do NOT want picky compilers down here
+# We do NOT want picky compilers down here
 CFLAGS = $(PMIX_CFLAGS_BEFORE_PICKY)
-PYTHONPATH=$PYTHONPATH:$(DESTDIR)$(prefix)/lib/python$(PYTHON_VERSION)/site-packages
 
-helpers = setup.py pmix.pyx pmix.pxi construct.py
+helpers = \
+        setup.py \
+        construct.py
 
-EXTRA_DIST = $(helpers)
+src_files = \
+        pmix.pxi \
+        pmix.pyx
+
+EXTRA_DIST = $(helpers) $(src_files)
 
 if WANT_PYTHON_BINDINGS
 
-install-exec-local: $(helpers)
+harvested_headers = \
+        $(top_srcdir)/include/pmix.h \
+        $(top_builddir)/include/pmix_common.h \
+        $(top_srcdir)/include/pmix_deprecated.h \
+        $(top_srcdir)/include/pmix_server.h \
+        $(top_srcdir)/include/pmix_tool.h
+
+constants_files = \
+        pmix_constants.pxd \
+        pmix_constants.pxi
+
+# Create pmix_constants.pxd and pmix_constants.pxi by harvesting a
+# bunch of PMIx header files These will be generated in the PMIx build
+# dir.
+$(constants_files): construct.py $(harvested_headers)
 	$(PYTHON) $(top_srcdir)/bindings/python/construct.py --src="$(top_builddir)/include" --include-dir="$(top_srcdir)/include"
-	PMIX_BINDINGS_TOP_SRCDIR=$(PMIX_TOP_SRCDIR) PMIX_BINDINGS_TOP_BUILDDIR=$(PMIX_TOP_BUILDDIR) \
-		$(PYTHON) $(top_srcdir)/bindings/python/setup.py build_ext --library-dirs="$(DESTDIR)$(libdir)" --user
-		if [ $(PMIX_TOP_SRCDIR) != $(PMIX_TOP_BUILDDIR) ]; then \
-			pushd $(PMIX_TOP_BUILDDIR)/bindings/python; \
-			rm -f setup.py; \
-			cp $(PMIX_TOP_SRCDIR)/bindings/python/setup.py .; \
-			popd; \
-		fi
-	PMIX_BINDINGS_TOP_SRCDIR=$(PMIX_TOP_SRCDIR) PMIX_BINDINGS_TOP_BUILDDIR=$(PMIX_TOP_BUILDDIR) \
-		$(PYTHON) -m pip install --prefix="$(DESTDIR)$(prefix)" --editable $(top_builddir)/bindings/python
 
+# Cython does not natively understand VPATH builds.  Specifically,
+# Cython will build things in the same tree where it finds
+# pmix.[pxi|pyx] (which is the PMIx source tree).
+#
+# Cython's "setup.py build_ext
+# --build-lib=SOMEWHERE_IN_THE_BUILD_TREE" *almost* does what we want.
+# But even though it makes the build/install trees in the directory we
+# tell it (vs. the source tree), it still writes pmix.c in the source
+# tree, which is definitely not what we want.
+#
+# We want to properly support VPATH builds, so we fake out Cython and
+# force it to build in the PMIx build tree by making sym links for
+# pmix.[pxi|pyx] from the source tree to the build tree.  Cython can
+# then find these files in the build tree, and build everything there
+# (including pmix.c).
+#
+# This is a little gross, but it works. :-/
+$(PMIX_TOP_BUILDDIR)/bindings/python/pmix.pxi:
+	$(LN_S) $(PMIX_TOP_SRCDIR)/bindings/python/pmix.pxi $(PMIX_TOP_BUILDDIR)/bindings/python/pmix.pxi
+$(PMIX_TOP_BUILDDIR)/bindings/python/pmix.pyx:
+	$(LN_S) $(PMIX_TOP_SRCDIR)/bindings/python/pmix.pyx $(PMIX_TOP_BUILDDIR)/bindings/python/pmix.pyx
+
+# When we run "setup.pu build_ext ...", several files/directories are
+# created.  The exact files / directory names may be system-dependent,
+# so we don't try to check for most of them.  Instead, we just check
+# for one sentinel file that is always created during this step:
+# pmix.c, and use that as a proxy for all the other things that were
+# created during this step.
+SENTINEL_FILE = pmix.c
+
+# Create $(SENTINEL_FILE) from its sources and the constants we
+# harvested from various PMIx header files.  Ensure to depend on the
+# pmix.[pxi|pyx] in the build tree so that those rules get triggered
+# and we get sym links over to the source tree, if necessary.
+$(SENTINEL_FILE): $(PMIX_TOP_BUILDDIR)/bindings/python/pmix.pxi
+$(SENTINEL_FILE): $(PMIX_TOP_BUILDDIR)/bindings/python/pmix.pyx
+$(SENTINEL_FILE): $(constants_files)
+	PMIX_BINDINGS_TOP_SRCDIR=$(PMIX_TOP_SRCDIR) PMIX_BINDINGS_TOP_BUILDDIR=$(PMIX_TOP_BUILDDIR) \
+		$(PYTHON) $(top_srcdir)/bindings/python/setup.py build_ext --library-dirs="$(PMIX_TOP_BUILDDIR)/src/.libs" --user
+
+BUILT_SOURCES = $(constants_files)
+
+# We don't put $(SENTINEL_FILE) BUILT_SOURCES because then "make dist"
+# will fail if it can't be created (e.g., if no suitable Python +
+# Cython + etc. is available) -- because the files in BUILT_SOURCES
+# are dependencies of "distdir".
+#
+# More specifically: $(SENTINEL_FILE) isn't shipped in the tarball, so
+# it doesn't seem right to require it to be able to be built for "make
+# dist" to succeed.  Hence, we (slightly) abuse Automake's "noinst"
+# and "DATA" to force $(SENTINEL_FILE) to be built during "make all"
+# when WANT_PYTHON_BINDINGS is true.
+noinst_DATA = $(SENTINEL_FILE)
+
+install-exec-local:
+	PYTHONPATH=$$PYTHONPATH:$(DESTDIR)$(pythondir):$(DESTDIR)$(pyexecdir) \
+		PMIX_BINDINGS_TOP_SRCDIR=$(PMIX_TOP_SRCDIR) PMIX_BINDINGS_TOP_BUILDDIR=$(PMIX_TOP_BUILDDIR) \
+		$(PYTHON) $(top_srcdir)/bindings/python/setup.py install --quiet --prefix="$(DESTDIR)$(prefix)"
+
+# Be careful with easy-install.pth -- we may not be the only entity
+# that put things in there.  Therefore, carefully remove the pmix
+# entry from it, and then remove the file altogether if is then
+# otherwise empty.  Finally, remove the installed pypmix Python egg.
 uninstall-hook:
-	rm -f $(pythondir)/pmix*.so
-	rm -f $(pythondir)/pypmix-*.egg-info
+	-@file=$(pythondir)/easy-install.pth; \
+	tmpfile=$(pythondir)/easy-install.pth.$$; \
+	if test -r $$file; then \
+	    grep -v /pypmix- $$file > $$tmpfile; \
+	    diff -q $$file $$tmpfile 2>&1 >/dev/null; \
+	    if test $$? -ne 0; then \
+	        cp $$tmpfile $$file; \
+	    fi; \
+	    rm -f $$tmpfile; \
+	    if test ! -s $$file; then \
+	        rm -f $$file; \
+	    fi; \
+	fi
+	rm -rf $(pythondir)/pypmix*
 
-CLEANFILES += pmix.c
+CLEANFILES += $(constants_files)
 
+# Also delete the other files and dirs that Cython creates during its
+# "setup.py build_ext ..." step.  We need to use clean-local because
+# the names in CLEANFILE are not "rm -rf"'ed.
 clean-local:
-	rm -rf build pypmix.egg-info dist
+	rm -rf $(SENTINEL_FILE) build dist pypmix.egg-info
 
-endif
+endif # WANT_PYTHON_BINDINGS

bindings/python/setup.py
@@ -2,24 +2,31 @@
 #
 # Copyright (c) 2022      Nanook Consulting. All rights reserved
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-from sys import platform, maxsize, version_info
+# Note: as of Dec 2022, we still use setup.py with the Python
+# setuptools package, even though setuptools docs now recommend
+# upgrading to the setup.cfg or pyproject.toml methods.  Perhaps
+# someday we'll migrate to the newer methods, but until then, setup.py
+# seems to be working.
+
 import os, sys
-from Cython.Compiler.Main import default_options, CompilationOptions
+import warnings
+import setuptools
+
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+from Cython.Compiler.Main import default_options
 default_options['emit_linenums'] = True
-from subprocess import check_output, CalledProcessError
 
 def get_include():
     dirs = []
-    try:
-        dirs.append(os.environ['PMIX_BINDINGS_TOP_BUILDDIR'] + "/include")
-        dirs.append(os.environ['PMIX_BINDINGS_TOP_SRCDIR'] + "/include")
-    except:
-        return dirs
+    for key in ['BUILDDIR', 'SRCDIR']:
+        name = f'PMIX_BINDINGS_TOP_{key}'
+        if name in os.environ:
+            dirs.append(f'{os.environ[name]}/include')
+
     return dirs
-    
+
 def getVersion():
     dir = os.path.dirname(__file__)
 
@@ -56,6 +63,28 @@ def package_setup(package_name, package_vers):
     '''
     Package setup routine.
     '''
+
+    # Find the Cython source file.  Note that we always look in the
+    # build tree.  The Makefile.am will ensure to always set this env
+    # variable, but if someone invokes this script without setting
+    # that env variable, just assume that pmix.pyx is in the same
+    # directory as this script.
+    key = 'PMIX_BINDINGS_TOP_BUILDDIR'
+    if key in os.environ:
+        dir = os.environ[key]
+        src_filename = os.path.join(dir, "bindings", "python", "pmix.pyx")
+    else:
+        dir = os.path.dirname(sys.argv[0])
+        src_filename = os.path.join(dir, "pmix.pyx")
+
+    # Per comment at the top of this file, we know that setup.py is
+    # deprecated/discouraged.  Until we switch away from it, remove
+    # the setuptools "deprecated" warnings that are emitted to stderr
+    # so that we do not scare the users.
+    if hasattr(setuptools, "SetuptoolsDeprecationWarning"):
+        warnings.filterwarnings("ignore",
+                                category=setuptools.SetuptoolsDeprecationWarning)
+
     setup(
         name = 'pypmix',
         version = getVersion(),
@@ -78,11 +107,14 @@ def package_setup(package_name, package_vers):
                 'Programming Language :: Python :: 3.10',
                 'Programming Language :: Python :: 3.11',
                 'Programming Language :: Python :: 3.12'],
-        keywords = 'PMI PMIx HPC MPI SHMEM',
+        keywords = ['PMI', 'PMIx', 'HPC', 'MPI', 'SHMEM' ],
         platforms = 'any',
+        install_requires = ["cython"],
+        zip_safe = False,
         ext_modules = cythonize([Extension("pmix",
-                                           [os.environ['PMIX_BINDINGS_TOP_SRCDIR']+"/bindings/python/pmix.pyx"],
-                                           libraries=["pmix"]) ],
+                                           [src_filename],
+                                           libraries=["pmix"],
+                                           depends=[], )],
                                 compiler_directives={'language_level': 3}),
         include_dirs = get_include()
     )