e6328142f66fb5f2c3f8d8ff342c057d830e4f42
Samuel K. Gutierrez <samuel@lanl.gov>
2023-02-24 09:11:22 -0700

    Plug memory leaks.
    Plug memory leaks identified by memcheck on the prterun side.
    
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>
    (cherry picked from commit 51b2010d8a7bf7558ba86ee338d494612e437798)

--- src/mca/bfrops/base/bfrop_base_macro_backers.c
+++ src/mca/bfrops/base/bfrop_base_macro_backers.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2022-2023 Nanook Consulting.  All rights reserved.
+ * Copyright (c) 2022-2023 Triad National Security, LLC. All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
