e9e4dbe7ce5a3b428f8dc42f6ac7a0919b57d919
Ralph Castain <rhc@pmix.org>
2023-04-11 09:27:45 -0700

    Ensure tools send requests to their server
    Tools may register server module function pointers to
    allow them to handle certain requests. The Python bindings
    are an example were the pointers can be registered, even
    though the host tool doesn't support them. Ensure that
    tools only use those pointers for requests that come
    up to them from clients, and don't call themselves back
    for those operations.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 02de3690ce41348e66a3cf8d796ddff18a996b05)

--- src/client/pmix_client_spawn.c
+++ src/client/pmix_client_spawn.c
@@ -8,7 +8,7 @@
  * Copyright (c) 2016      Mellanox Technologies, Inc.
  *                         All rights reserved.
  * Copyright (c) 2016      IBM Corporation.  All rights reserved.
- * Copyright (c) 2021-2022 Nanook Consulting.  All rights reserved.
+ * Copyright (c) 2021-2023 Nanook Consulting.  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -76,20 +76,13 @@ PMIX_EXPORT pmix_status_t PMIx_Spawn(const pmix_info_t job_info[], size_t ninfo,
 
     PMIX_ACQUIRE_THREAD(&pmix_global_lock);
 
-    pmix_output_verbose(2, pmix_client_globals.spawn_output, "%s pmix: spawn called",
+    pmix_output_verbose(2, pmix_client_globals.spawn_output,
+                        "%s pmix: spawn called",
                         PMIX_NAME_PRINT(&pmix_globals.myid));
 
     if (pmix_globals.init_cntr <= 0) {
         PMIX_RELEASE_THREAD(&pmix_global_lock);
         return PMIX_ERR_INIT;
-    }
-
-    /* if we aren't connected, don't attempt to send */
-    if (!PMIX_PEER_IS_SERVER(pmix_globals.mypeer) &&
-        !pmix_globals.connected &&
-        !PMIX_PEER_IS_LAUNCHER(pmix_globals.mypeer)) {
-        PMIX_RELEASE_THREAD(&pmix_global_lock);
-        return PMIX_ERR_UNREACH;
     }
     PMIX_RELEASE_THREAD(&pmix_global_lock);
 
@@ -158,7 +151,8 @@ PMIX_EXPORT pmix_status_t PMIx_Spawn_nb(const pmix_info_t job_info[], size_t nin
         /* if I am a launcher, we default to local fork/exec */
         if (PMIX_PEER_IS_LAUNCHER(pmix_globals.mypeer)) {
             forkexec = true;
-        } else if (!PMIX_PEER_IS_SERVER(pmix_globals.mypeer)) {
+        } else if (!PMIX_PEER_IS_SERVER(pmix_globals.mypeer) ||
+                   PMIX_PEER_IS_TOOL(pmix_globals.mypeer)) {
             PMIX_RELEASE_THREAD(&pmix_global_lock);
             return PMIX_ERR_UNREACH;
         }
@@ -260,7 +254,8 @@ PMIX_EXPORT pmix_status_t PMIx_Spawn_nb(const pmix_info_t job_info[], size_t nin
 
     /* if we are a server, then process this ourselves */
     if (PMIX_PEER_IS_SERVER(pmix_globals.mypeer) &&
-        !PMIX_PEER_IS_LAUNCHER(pmix_globals.mypeer)) {
+        !PMIX_PEER_IS_LAUNCHER(pmix_globals.mypeer) &&
+        !PMIX_PEER_IS_TOOL(pmix_globals.mypeer)) {
 
         if (NULL == pmix_host_server.spawn) {
             return PMIX_ERR_NOT_SUPPORTED;
