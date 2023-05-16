77136e759c76e59fc7a614daee5c7c544f2b9ec7
Samuel K. Gutierrez <samuel@lanl.gov>
2023-05-16 09:17:52 -0600

    Fix compilation error.
    Apply patch from George Bosilca to fix compilation error he reported.
    
    Co-authored-by: George Bosilca <bosilca@icl.utk.edu>
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

--- src/include/pmix_stdint.h
+++ src/include/pmix_stdint.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2004-2007 The Trustees of Indiana University and Indiana
  *                         University Research and Technology
  *                         Corporation.  All rights reserved.
- * Copyright (c) 2004-2005 The University of Tennessee and The University
+ * Copyright (c) 2004-2023 The University of Tennessee and The University
  *                         of Tennessee Research Foundation.  All rights
  *                         reserved.
  * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart,
@@ -16,6 +16,7 @@
  *                         and Technology (RIST). All rights reserved.
  * Copyright (c) 2018      Intel, Inc.  All rights reserved.
  * Copyright (c) 2021-2022 Nanook Consulting.  All rights reserved.
+ * Copyright (c) 2023      Triad National Security, LLC. All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -30,7 +31,7 @@
 #ifndef PMIX_STDINT_H
 #define PMIX_STDINT_H 1
 
-#include "pmix_config.h"
+#include "src/include/pmix_config.h"
 
 /*
  * Include what we can and define what is missing.
