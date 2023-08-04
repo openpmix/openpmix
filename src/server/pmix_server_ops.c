b76e390f57e2c3aaee6a0ffe54448ad0042200e4
Ralph Castain <rhc@pmix.org>
2023-08-04 06:33:22 -0600

    Remove debug
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/server/pmix_server_ops.c
+++ src/server/pmix_server_ops.c
@@ -2681,8 +2681,7 @@ pmix_status_t pmix_server_query(pmix_peer_t *peer, pmix_buffer_t *buf,
     pmix_status_t rc;
     pmix_query_caddy_t *cd;
 
-  //  pmix_output_verbose(2, pmix_server_globals.base_output,
-    pmix_output(0,
+    pmix_output_verbose(2, pmix_server_globals.base_output,
                         "recvd query from client");
 
     cd = PMIX_NEW(pmix_query_caddy_t);
@@ -2715,7 +2714,6 @@ pmix_status_t pmix_server_query(pmix_peer_t *peer, pmix_buffer_t *buf,
             return rc;
         }
     }
-    pmix_output(0, "%s:%s:%d", __FILE__, __func__, __LINE__);
     PMIX_THREADSHIFT(cd, pmix_parse_localquery);
     return PMIX_SUCCESS;
 }
