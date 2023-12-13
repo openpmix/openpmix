e6ba079bf0df236f9b7958d6fbc2f9bdc16f70e8
Ralph Castain <rhc@pmix.org>
2023-12-12 18:26:42 -0700

    Remove use of stat from pmix_getcwd
    Avoid use of the stat function
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/util/pmix_getcwd.c
+++ src/util/pmix_getcwd.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2007-2020 Cisco Systems, Inc.  All rights reserved
  * Copyright (c) 2019      Intel, Inc.  All rights reserved.
- * Copyright (c) 2021-2022 Nanook Consulting.  All rights reserved.
+ * Copyright (c) 2021-2023 Nanook Consulting  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -38,7 +38,6 @@ int pmix_getcwd(char *buf, size_t size)
 {
     char cwd[PMIX_PATH_MAX];
     char *pwd = getenv("PWD");
-    struct stat a, b;
     char *shortened;
 
     /* Bozo checks (e.g., if someone accidentally passed -1 to the
@@ -52,39 +51,16 @@ int pmix_getcwd(char *buf, size_t size)
         return PMIX_ERR_IN_ERRNO;
     }
 
-#if !defined(HAVE_SYS_STAT_H)
-    /* If we don't have stat(), then we can't tell if the $PWD and cwd
-       are the same, so just fall back to getcwd(). */
-    pwd = cwd;
-#else
     if (NULL == pwd) {
         pwd = cwd;
     } else {
-        /* If the two are not the same value, figure out if they are
-           pointing to the same place */
         if (0 != strcmp(pwd, cwd)) {
-            /* If we can't stat() what getcwd() gave us, give up */
-            if (0 != stat(cwd, &a)) {
-                return PMIX_ERR_IN_ERRNO;
-            }
-            /* If we can't stat() $PWD, then $PWD could just be stale
-               -- so ignore it. */
-            else if (0 != stat(pwd, &b)) {
-                pwd = cwd;
-            }
-            /* Otherwise, we successfully stat()'ed them both, so
-               compare.  If either the device or inode is not the
-               same, then fallback to getcwd(). */
-            else {
-                if (a.st_dev != b.st_dev || a.st_ino != b.st_ino) {
-                    pwd = cwd;
-                }
-            }
+            // fallback to getcwd value
+            pwd = cwd;
         }
     }
-#endif
 
-    /* If we got here, pwd is pointing to the result that we want to
+    /* pwd is pointing to the result that we want to
        give.  Ensure the user's buffer is long enough.  If it is, copy
        in the value and be done. */
     if (strlen(pwd) > size) {
