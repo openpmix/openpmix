5e289715a0399e2d12e64860edff50ba9431ece3
Ralph Castain <rhc@pmix.org>
2023-12-14 17:56:12 -0700

    Do not remove the system tmpdir during cleanup
    Ensure os_dirpath_destroy protects the system tmpdir
    from removal.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 6381f6c59094e231c179c7233a036bfbb6b42028)

--- src/util/pmix_cmd_line.h
+++ src/util/pmix_cmd_line.h
@@ -123,6 +123,7 @@ PMIX_CLASS_DECLARATION(pmix_cli_result_t);
 #define PMIX_CLI_TIMEOUT                "timeout"                   // required
 #define PMIX_CLI_TMPDIR                 "tmpdir"                    // required
 #define PMIX_CLI_CONNECTION_ORDER       "connect-order"             // required
+#define PMIX_CLI_SYS_CONTROLLER         "system-controller"         // none
 
 // Allocation request options
 #define PMIX_CLI_REQ_ID                 "request-id"                // required
