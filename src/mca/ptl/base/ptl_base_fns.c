cf70e1c978747b34f5cb764bd95fe405645a4228
Ralph Castain <rhc@pmix.org>
2023-12-19 01:11:25 -0700

    Cleanup a few typos and remove debug output
    Remove debug output. Correctly use our own peer
    (instead of the one passed to us) when constructing
    a connection handshake message.
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 8183fde19dcf90ed4ebbe68e79e3b4bf0ad2dc70)

--- src/mca/ptl/base/ptl_base_fns.c
+++ src/mca/ptl/base/ptl_base_fns.c
@@ -52,6 +52,8 @@
 
 /****    SUPPORTING FUNCTIONS    ****/
 static void timeout(int sd, short args, void *cbdata);
+static pmix_status_t construct_message(pmix_peer_t *peer, char **msgout, size_t *sz,
+                                       pmix_info_t *iptr, size_t niptr);
 
 pmix_status_t pmix_ptl_base_set_peer(pmix_peer_t *peer, char *evar)
 {
@@ -526,7 +528,7 @@ static pmix_status_t send_connect_ack(pmix_peer_t *peer,
     peer->proc_type.flag = pmix_ptl_base_set_flag(&sdsize);
 
     /* construct the contact message */
-    rc = pmix_ptl_base_construct_message(peer, &msg, &sdsize, iptr, niptr);
+    rc = construct_message(peer, &msg, &sdsize, iptr, niptr);
     if (PMIX_SUCCESS != rc) {
         PMIX_ERROR_LOG(rc);
         return rc;
@@ -762,8 +764,8 @@ pmix_rnd_flag_t pmix_ptl_base_set_flag(size_t *sz)
     return flag;
 }
 
-pmix_status_t pmix_ptl_base_construct_message(pmix_peer_t *peer, char **msgout, size_t *sz,
-                                              pmix_info_t *iptr, size_t niptr)
+static pmix_status_t construct_message(pmix_peer_t *peer, char **msgout, size_t *sz,
+                                       pmix_info_t *iptr, size_t niptr)
 {
     char *msg;
     char *sec, *bfrops, *gds;
