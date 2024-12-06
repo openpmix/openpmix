e106fec9c14a13fa5d8a02387c292a3b8e3ef02e
Ralph Castain <rhc@pmix.org>
2024-12-06 12:46:30 -0700

    Remove prm framework as not currently implemented
    Remains on master branch in case there is interest
    in the future
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 4f26b5680c3cde2dfa7e6e28c39d0bd7a56591c4)

--- src/runtime/pmix_init.c
+++ src/runtime/pmix_init.c
@@ -46,7 +46,6 @@
 #include "src/mca/plog/base/base.h"
 #include "src/mca/pnet/base/base.h"
 #include "src/mca/preg/base/base.h"
-#include "src/mca/prm/base/base.h"
 #include "src/mca/psec/base/base.h"
 #include "src/mca/ptl/base/base.h"
 #include "src/util/pmix_name_fns.h"
@@ -530,19 +529,6 @@ int pmix_rte_init(uint32_t type, pmix_info_t info[], size_t ninfo, pmix_ptl_cbfu
     }
     if (PMIX_SUCCESS != (ret = pmix_plog_base_select())) {
         error = "pmix_plog_base_select";
-        goto return_error;
-    }
-
-    /* open and initialize */
-    ret = pmix_mca_base_framework_open(&pmix_prm_base_framework, PMIX_MCA_BASE_OPEN_DEFAULT);
-    if (PMIX_SUCCESS != ret) {
-        error = "pmix_prm_base_open";
-        goto return_error;
-    }
-
-    ret = pmix_prm_base_select();
-    if (PMIX_SUCCESS != ret) {
-        error = "pmix_prm_base_select";
         goto return_error;
     }
 
