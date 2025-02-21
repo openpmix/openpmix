f92a222105c954c5b18f7da06357a0b1a2cb6100
Ralph Castain <rhc@pmix.org>
2025-02-21 09:50:42 -0700

    Minor cleanup
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/client/pmix_client_get.c
+++ src/client/pmix_client_get.c
@@ -8,7 +8,7 @@
  * Copyright (c) 2016-2018 Mellanox Technologies, Inc.
  *                         All rights reserved.
  * Copyright (c) 2016-2022 IBM Corporation.  All rights reserved.
- * Copyright (c) 2021-2024 Nanook Consulting  All rights reserved.
+ * Copyright (c) 2021-2025 Nanook Consulting  All rights reserved.
  * Copyright (c) 2023      Triad National Security, LLC. All rights reserved.
  * $COPYRIGHT$
  *
@@ -1308,7 +1308,7 @@ static pmix_status_t refresh_cache(const pmix_proc_t *p)
     PMIX_PTL_SEND_RECV(rc, pmix_client_globals.myserver, msg, refcb, (void *)cb);
     if (PMIX_SUCCESS != rc) {
         PMIX_ERROR_LOG(rc);
-        PMIX_DESTRUCT(&cb);
+        PMIX_RELEASE(cb);
         return rc;
     }
     PMIX_WAIT_THREAD(&cb->lock);
