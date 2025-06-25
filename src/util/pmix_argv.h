9d06ac166cbf8da6ca9365eab703771b7faaea3a
Ralph Castain <rhc@pmix.org>
2025-06-25 13:28:27 -0600

    Try reorganizing
    Signed-off-by: Ralph Castain <rhc@pmix.org>
    (cherry picked from commit 66743da9e6cbfa21c45a3ffcfea62451cf6b4600)

--- src/util/pmix_argv.h
+++ src/util/pmix_argv.h
@@ -17,7 +17,7 @@
  *
  * Copyright (c) 2015-2019 Research Organization for Information Science
  *                         and Technology (RIST).  All rights reserved.
- * Copyright (c) 2021-2022 Nanook Consulting  All rights reserved.
+ * Copyright (c) 2021-2025 Nanook Consulting  All rights reserved.
  * $COPYRIGHT$
  *
  * Additional copyrights may follow
@@ -41,7 +41,7 @@
 #    include <sys/types.h>
 #endif
 
-#include "pmix_common.h"
+#include "pmix.h"
 
 BEGIN_C_DECLS
 
