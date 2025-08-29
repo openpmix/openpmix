947d75e14b7bdaec5db48da515a3066bde6a698b
Ralph Castain <rhc@pmix.org>
2025-08-28 20:06:40 -0600

    Cleanup a bit on connection handling
    Now that tools are part of the tracked nspaces, need to be
    a little more careful about appending them to the trackers.
    Modify the toolqry example to avoid picking up system tools
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- src/mca/ptl/base/ptl_base_connection_hdlr.c
+++ src/mca/ptl/base/ptl_base_connection_hdlr.c
@@ -809,6 +809,11 @@ static void process_cbfunc(int sd, short args, void *cbdata)
         goto error;
     }
 
+pmix_namespace_t *foo;
+PMIX_LIST_FOREACH(foo, &pmix_globals.nspaces, pmix_namespace_t) {
+    pmix_output(0, "NSPACE %s", foo->nspace);
+}
+pmix_output(0, "\n\n");
     /* set the socket non-blocking for all further operations */
     pmix_ptl_base_set_nonblocking(pnd->sd);
 
@@ -946,7 +951,6 @@ static pmix_status_t process_tool_request(pmix_pending_connection_t *pnd,
             nptr->version.major = pnd->proc_type.major;
             nptr->version.minor = pnd->proc_type.minor;
             nptr->version.release = pnd->proc_type.release;
-            pmix_list_append(&pmix_globals.nspaces, &nptr->super);
             pnd->nspace_created = true;
         }
         /* now look for the rank */
@@ -1032,6 +1036,10 @@ static pmix_status_t process_tool_request(pmix_pending_connection_t *pnd,
             PMIX_RELEASE(peer);
             return PMIX_ERR_NOMEM;
         }
+        if (!pnd->need_id) {
+            // must have been given one
+            nptr->nspace = strdup(pnd->proc.nspace);
+        }
         /* save the version */
         nptr->version.major = pnd->proc_type.major;
         nptr->version.minor = pnd->proc_type.minor;
@@ -1041,6 +1049,7 @@ static pmix_status_t process_tool_request(pmix_pending_connection_t *pnd,
          * from the host's upcall since they can/will assign the
          * tool with a namespace */
         PMIX_RETAIN(nptr);
+
     }
 
     peer->nptr = nptr;
