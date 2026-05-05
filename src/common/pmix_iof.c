4216db2041dfb9262c2747d64a0201295544cbd4
Jakub Benda <albandil@atlas.cz>
2026-05-05 11:36:44 +0200

    Prepend I/O residuals to correct redirected streams
    A stored I/O residual is included in the redirected stream of a given
    process only when *both* the name of the process *and* the stream kind
    match the residual metadata. This prevents cross-contamination of
    redirected outputs between distinct processes.
    
    Signed-off-by: Jakub Benda <albandil@atlas.cz>
    (cherry picked from commit 9a5c62d07bf3de9190440a87c3005afb6106a3d9)

--- src/common/pmix_iof.c
+++ src/common/pmix_iof.c
@@ -1650,22 +1650,30 @@ pmix_status_t pmix_iof_write_output(const pmix_proc_t *name,
         return rc;
     }
 
-    /* see if we have some residual for this name/stream */
-    inputdata = bo->bytes;
-    inputsize = bo->size;
-    copied = false;
-    PMIX_LIST_FOREACH(res, &pmix_server_globals.iof_residuals, pmix_iof_residual_t) {
-        if (PMIX_CHECK_PROCID(name, &res->name) || (stream & res->stream)) {
-            /* we need to pre-pend the residual data to the new
-             * data so any lines can be completed */
-            inputdata = (char*)malloc(inputsize + res->bo.size);
-            memcpy(inputdata, res->bo.bytes, res->bo.size);
-            memcpy(&inputdata[res->bo.size], bo->bytes, bo->size);
-            inputsize += res->bo.size;
-            copied = true;
-            pmix_list_remove_item(&pmix_server_globals.iof_residuals, &res->super);
-            PMIX_RELEASE(res);
-            break;
+    if (myflags.raw) {
+        rc = write_output_line(name, channel, &myflags, stream,
+                               copystdout, copystderr, bo);
+        return rc;
+
+    } else {
+
+        /* see if we have some residual for this name/stream */
+        inputdata = bo->bytes;
+        inputsize = bo->size;
+        copied = false;
+        PMIX_LIST_FOREACH(res, &pmix_server_globals.iof_residuals, pmix_iof_residual_t) {
+            if (PMIX_CHECK_PROCID(name, &res->name) && (stream & res->stream)) {
+                /* we need to pre-pend the residual data to the new
+                 * data so any lines can be completed */
+                inputdata = (char*)malloc(inputsize + res->bo.size);
+                memcpy(inputdata, res->bo.bytes, res->bo.size);
+                memcpy(&inputdata[res->bo.size], bo->bytes, bo->size);
+                inputsize += res->bo.size;
+                copied = true;
+                pmix_list_remove_item(&pmix_server_globals.iof_residuals, &res->super);
+                PMIX_RELEASE(res);
+                break;
+            }
         }
     }
 
