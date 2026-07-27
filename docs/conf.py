39311f701e4800360aa3d495444a236914e6f525
Ralph Castain <rhc@pmix.org>
2026-07-27 11:49:49 -0600

    Remove stale recommonmark dependency
    This Python module has been sunsetted and isn't being used
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- docs/conf.py
+++ docs/conf.py
@@ -129,7 +129,7 @@ if key in os.environ and os.environ[key] == 'True':
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 import sphinx_rtd_theme
-extensions = ['recommonmark', "sphinx_rtd_theme"]
+extensions = ["sphinx_rtd_theme"]
 pygments_style = 'sphinx'
 
 # Add any paths that contain templates here, relative to this directory.
