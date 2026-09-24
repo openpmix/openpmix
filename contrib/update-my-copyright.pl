d88ac81033744ecd5834690dcff94f2121853b8b
Jeff Squyres <jeff@squyres.com>
2026-09-24 15:16:48 -0400

    update-my-copyright.pl: match @protected against top-level paths
    find_modified_files() returns file names relative to the current
    directory, but the @protected patterns are written relative to the
    top-level directory ("contrib/update-my-copyright.pl",
    "config/oac/").  So the protection only worked when the script was
    run from the top-level directory: run from contrib/, the script's own
    name comes back as "update-my-copyright.pl", does not match, and the
    script updates the copyright in itself.
    
    Prepend the current directory's path relative to the top-level
    directory (from "git rev-parse --show-prefix") before matching the
    patterns.
    
    The same problem was found by Copilot review on the corresponding
    PRRTE change (openpmix/prrte#2827).  It is not new: the matching has
    always used the current-directory-relative name.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>
    (cherry picked from commit 6d4a42cad37eb96fa5789f0e22c5c4fdbbf02022)

--- contrib/update-my-copyright.pl
+++ contrib/update-my-copyright.pl
@@ -4,6 +4,7 @@
 # Copyright (c) 2016-2017 Intel, Inc. All rights reserved.
 # Copyright (c) 2017      IBM Corporation. All rights reserved.
 # Copyright (c) 2026      Nanook Consulting  All rights reserved.
+# Copyright (c) 2026      Jeffrey M. Squyres.  All rights reserved.
 # $COPYRIGHT$
 #
 
@@ -133,6 +134,10 @@ quiet_print "==> This year: $year\n";
 my $start = cwd();
 my $top = `git rev-parse --show-toplevel`;
 chomp($top);
+# The current directory relative to $top (e.g., "contrib/"), or the
+# empty string at the top-level directory
+my $prefix = `git rev-parse --show-prefix`;
+chomp($prefix);
 
 quiet_print "==> Top-level repository dir: $top\n";
 quiet_print "==> Current directory: $start\n";
@@ -150,10 +155,13 @@ foreach my $f (@files) {
     # ignore embedded copies of external codes as we shouldn't
     # be overwriting their copyrights - if someone actually
     # modified any of those files, they can manually update
-    # the copyright
+    # the copyright.  $f is relative to the current directory, but the
+    # @protected patterns are relative to the top-level directory, so
+    # match against the latter.
+    my $top_relative = "$prefix$f";
     my $ignore = 0;
     foreach my $p (@protected) {
-        if (eval("\$f =~ /$p/")) {
+        if (eval("\$top_relative =~ /$p/")) {
             quiet_print "Ignoring protected file $f\n";
             $ignore = 1;
             last;
